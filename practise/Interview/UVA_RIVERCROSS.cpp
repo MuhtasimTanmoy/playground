318c02baebed53bfb348b82995345f49ad1498c5
MuhtasimTanmoy <taaanmo0oy@gmail.com>
2019-11-27 14:12:04 +0600

    UVA add

---
+++ practise/Interview/UVA_RIVERCROSS.cpp
@@ -0,0 +1,40 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define ll long long
+#define ull unsigned long long
+#define ff first
+#define ss second
+#define mp make_pair
+#define pb push_back
+
+const int N = 2e5 + 11;
+
+void solve()
+{
+	double d, v, u;
+	cin >> d >> v >> u;
+
+	double whenPerpendicular = sqrt(u * u - v * v);
+	double whenStraight = u;
+	
+
+	cout<< abs((d/whenStraight) - (d/whenPerpendicular))<<endl;
+}
+
+int main()
+{
+	freopen("input.txt", "r", stdin);
+	ios_base::sync_with_stdio(0);
+	cin.tie(0);
+	cout.tie(0);
+	int no_of_test_cases;
+	cin >> no_of_test_cases;
+	// no_of_test_cases = 1;
+	while (no_of_test_cases--)
+	{
+		solve();
+	}
+
+	return 0;
+}
