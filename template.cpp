b29e8ea8d4089c1bc3b9b189f0a10f21c4eb1e65
MuhtasimTanmoy <taaanmo0oy@gmail.com>
2021-05-31 06:37:54 +0600

    Update

--- template.cpp
+++ template.cpp
@@ -1,18 +1,7 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-#define ll long long
-#define ull unsigned long long
-#define ff first
-#define ss second
-#define mp make_pair
-#define pb push_back
-
-const int N = 2e5 + 11;
-
-void solve()
-{
-
+void solve() {
 	
 }
 
@@ -22,9 +11,7 @@ int main()
 	cin.tie(0);
 	cout.tie(0);
 	int no_of_test_cases = 0;
-	// cin >> no_of_test_cases;
-	if (!no_of_test_cases)
-		no_of_test_cases = 1;
+	cin >> no_of_test_cases;
 	while (no_of_test_cases--)
 	{
 		solve();    
