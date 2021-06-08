5d4853a3bf9e963944e4802b3d04104ff09ab432
MuhtasimTanmoy <taaanmo0oy@gmail.com>
2021-06-08 09:49:49 +0600

    https://codeforces.com/contest/493/problem/A

--- template.cpp
+++ template.cpp
@@ -1,21 +1,8 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-void solve() {
-	
-}
-
 int main()
 {
 	freopen("input.txt", "r", stdin);
-	cin.tie(0);
-	cout.tie(0);
-	int no_of_test_cases = 0;
-	cin >> no_of_test_cases;
-	while (no_of_test_cases--)
-	{
-		solve();    
-	}
-
 	return 0;
 }
