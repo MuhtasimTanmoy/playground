4877ba2941d3e4f4a02681e2cfffb62890dcb46f
Muhtasim Ulfat tanmoy <mtanmoy5086@gmail.com>
2021-06-23 11:22:03 +0600

    Time: 4 ms (91.78%), Space: 13 MB (41.80%) - LeetHub

---
+++ leetcode/best-time-to-buy-and-sell-stock-ii/best-time-to-buy-and-sell-stock-ii.cpp
@@ -0,0 +1,9 @@
+class Solution {
+public:
+    int maxProfit(vector<int>& prices) {
+        int profit{0};
+        for(int i=0;i<prices.size()-1;i++)
+            if(prices[i+1]>prices[i]) profit+=(prices[i+1]-prices[i]);
+        return prices.size()<=1?0:profit;
+    }
+};
