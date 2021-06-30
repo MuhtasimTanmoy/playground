5b021526cb935dfdb9c92be931400f61bbe352fe
MuhtasimTanmoy <taaanmo0oy@gmail.com>
2021-06-30 21:17:09 +0600

    Update structure"

--- C.cpp
+++ C.cpp
@@ -1,40 +1,10 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-int modulo = 10 * 10 * 10 * 10 * 10 * 10 * 10 * 10 * 10 + 7;
-
 int main()
 {
-    ios::sync_with_stdio(false);
+	ios::sync_with_stdio(false);
     cin.tie(0);
 	freopen("input.txt", "r", stdin);
-
-    int testCase;
-    cin>> testCase;
-
-    while(testCase--) {
-        int nums;
-        cin>>nums;
-        int secondRowNums;
-        secondRowNums = nums;
-
-        set<int> firstRow;
-
-        while (nums--)
-        {
-           int number;
-           cin>>number;
-           firstRow.insert(number);
-        }
-
-        int match
-        while (secondRowNums--)
-        {
-            int number;
-            cin>>number;
-            if (set.count(number) == )
-        }
-        
-    }
 	return 0;
 }
--- D.cpp
+++ D.cpp
@@ -1,46 +1,10 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-int jump(vector<int> &nums)
-{
-
-    int stepsNeeded = 0;
-
-    int initialTargetIndex = nums.size() - 1;
-
-    function<int(int)> canBeReachedFromAtMost = [&](int targetIndex) -> int
-    {
-        int canBeReachedFromAtMostIndex = targetIndex - 1;
-
-        int checkIndex = targetIndex - 1;
-
-        while (checkIndex >= 0)
-        {
-            int distance = targetIndex - checkIndex;
-            if (distance <= nums[checkIndex])
-            {
-                canBeReachedFromAtMostIndex = checkIndex;
-            }
-        }
-        return canBeReachedFromAtMostIndex;
-    };
-
-    while (canBeReachedFromAtMost(initialTargetIndex) != 0)
-    {
-        cout<<"Steps"<<endl;
-        stepsNeeded++;
-    }
-
-    return stepsNeeded;
-}
-
 int main()
 {
-    ios::sync_with_stdio(false);
+	ios::sync_with_stdio(false);
     cin.tie(0);
-    // freopen("input.txt", "r", stdin);
-
-    vector<int> list { 2,3,1,1,4 };
-    // jump(list);
-    return 0;
+	freopen("input.txt", "r", stdin);
+	return 0;
 }
