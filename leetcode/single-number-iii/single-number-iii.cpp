46c19a6fe8e18afa9c8da6b97ecf137972d0747f
Muhtasim Ulfat tanmoy <taaanmo0oy@gmail.com>
2021-07-03 00:07:10 +0600

    Update single-number-iii.cpp

--- leetcode/single-number-iii/single-number-iii.cpp
+++ leetcode/single-number-iii/single-number-iii.cpp
@@ -2,19 +2,46 @@ class Solution {
 public:
     vector<int> singleNumber(vector<int>& nums) {
         
-        vector<int> result;
-        map<int, int> occurenceCount;
-        
-        for( int i = 0; i < nums.size(); i++) {
-            occurenceCount[nums[i]]+=1;
+        long long twoNumbers = 0;
+        for(int i: nums) {
+            twoNumbers ^= i;
         }
         
-        for( auto occurence: occurenceCount) {
-            if (occurence.second == 1) {
-                result.push_back(occurence.first);
+        long long lastSetBit = twoNumbers & -twoNumbers;
+        
+        int first = 0;
+        int second = 0;
+        
+        for(int i: nums) {
+            if ( (long long) i & lastSetBit) {
+                first^=i;
+            } else {
+                second^=i;
             }
         }
         
-        return result;
+        return { first, second };
     }
 };
+
+
+
+// Set based approach
+
+// vector<int> singleNumber(vector<int>& nums) {
+        
+//         vector<int> result;
+//         map<int, int> occurenceCount;
+        
+//         for( int i = 0; i < nums.size(); i++) {
+//             occurenceCount[nums[i]] += 1;
+//         }
+        // a
+//         for( auto occurencea: occurenceCount) {
+//             if (occurence.second == 1) {
+//                 result.push_back(occurence.first);
+//             }
+//         }
+        
+//         return result;
+//     }
