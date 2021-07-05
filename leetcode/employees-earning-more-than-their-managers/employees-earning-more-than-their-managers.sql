617a033ab81a7c77528958db20520fd617a4519b
Muhtasim Ulfat tanmoy <taaanmo0oy@gmail.com>
2021-07-05 21:03:07 +0600

    Update employees-earning-more-than-their-managers.sql

--- leetcode/employees-earning-more-than-their-managers/employees-earning-more-than-their-managers.sql
+++ leetcode/employees-earning-more-than-their-managers/employees-earning-more-than-their-managers.sql
@@ -3,3 +3,9 @@
 select E.Name as Employee from Employee as E left join Employee as M 
 on E.ManagerId = M.Id 
 where E.Salary > M.Salary
+
+# Alternate approach
+
+select name `Employee` from Employee e
+where ManagerId is not null and
+Salary > (select Salary from Employee where Id = e.ManagerId)
