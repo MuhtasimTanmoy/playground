fe5b814eb516e8c1b4de1488cab1d7da08b7fa80
Muhtasim Ulfat tanmoy <mtanmoy5086@gmail.com>
2021-07-10 15:51:09 +0600

    Time: 0 ms (100.00%), Space: 5.9 MB (49.91%) - LeetHub

---
+++ leetcode/check-if-number-is-a-sum-of-powers-of-three/check-if-number-is-a-sum-of-powers-of-three.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    bool checkPowersOfThree(int n) {
+        
+        while( n > 0 ) {
+            
+            if( n % 3 == 2) {
+                return false;
+            }
+            
+            n = n / 3;
+        }
+        return true;
+    }
+};
