0479b1efe4382d5976803f8a17591f8fba52168f
MuhtasimTanmoy <taaanmo0oy@gmail.com>
2022-03-11 22:09:48 +0600

    Update

--- codeforces.com/contest/1312/C.cpp
+++ codeforces.com/contest/1312/C.cpp
@@ -1,5 +1,4 @@
-//https://1312/problem/C
-#include <bits/stdc++.h>
+\#include <bits/stdc++.h>
 using namespace std;
 
 #define ll long long
@@ -72,4 +71,83 @@ int main()
 	}
 
 	return 0;
+}
+
+
+
+
+// Alternative
+
+#include <bits/stdc++.h>
+using namespace std;
+
+#define log(x) cout << #x << " = " << x << endl;
+
+void execute() {
+    int num_of_elem, multiplier;
+    unordered_set<int> bag;
+    cin>> num_of_elem >> multiplier;
+    int element;
+
+    // get multiplier elements that it is made of
+    auto get_multipilers = [multiplier](int element) -> vector<int> {
+
+        int multipler_power = 0;
+        vector<int> list;
+        int count = 0;
+
+        if (element == 0) {
+            list.emplace_back(1);
+        }
+
+        while (element) {
+
+            if (element % multiplier > 1) {
+                
+            } else if (element % multiplier == 1) {
+                list.emplace_back(count);
+            }
+
+            count++;
+            element = element / multiplier;
+        }
+
+        return list;
+    };
+
+    while(num_of_elem--) {
+
+        vector<int> multipilers = get_multipilers(element);
+
+        for (auto multiplier :multipilers) {
+            cout<<multiplier<<" "; 
+        }
+        cout<<endl;
+        
+        for (auto multiplier :multipilers) {
+            if (bag.count(multiplier)) {
+                cout<<"NO"<<endl;
+                return;
+            } else {
+                bag.insert(multiplier);
+            }
+        }
+    }
+
+    cout<<"YES"<<endl;
+}
+
+int main() {
+
+	ios::sync_with_stdio(false);
+    cin.tie(0);
+	freopen("input.txt", "r", stdin);
+    int no_of_test_cases = 1;
+	cin >> no_of_test_cases;
+    while (no_of_test_cases--)
+	{
+		execute(); 
+	}
+	return 0;
+    
 }
---
+++ problem.cpp
@@ -0,0 +1,16 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define log(x) cout << #x << " = " << x << endl;
+
+
+
+int main()
+{
+	ios::sync_with_stdio(false);
+    cin.tie(0);
+	freopen("input.txt", "r", stdin);
+	return 0;
+}
+
+
