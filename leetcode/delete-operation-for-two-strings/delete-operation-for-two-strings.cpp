07fa8bf2a46c5cab5e35794f82a4e88e39293087
Muhtasim Ulfat tanmoy <mtanmoy5086@gmail.com>
2022-03-23 06:36:19 +0600

    Time: 28 ms (50.99%), Space: 6.7 MB (98.50%) - LeetHub

--- leetcode/delete-operation-for-two-strings/delete-operation-for-two-strings.cpp
+++ leetcode/delete-operation-for-two-strings/delete-operation-for-two-strings.cpp
@@ -3,32 +3,53 @@ public:
     int minDistance(string word1, string word2) {
         
         // longest common subsequence
+                
+//         int word1Len = word1.size();
+//         int word2Len = word2.size();
         
-        int word1Len = word1.size();
-        int word2Len = word2.size();
+//         vector<vector<int>> dp(word1Len + 1,
+//                                vector<int>(word2Len + 1, 0)
+//                               );
         
-        vector<vector<int>> dp(word1Len + 1,
-                               vector<int>(word2Len + 1, 0)
-                              );
-        
-        for (int i = 1; i<= word1Len; i++) {
+//         for (int i = 1; i<= word1Len; i++) {
             
-            for (int j = 1; j<= word2Len; j++) {
+//             for (int j = 1; j<= word2Len; j++) {
                 
-                if ( word1[i - 1] == word2[ j - 1] ) {
+//                 if ( word1[i - 1] == word2[ j - 1] ) {
                     
-                    dp[i][j] = dp[i-1][j-1] + 1;
+//                     dp[i][j] = dp[i-1][j-1] + 1;
                     
-                } else {
+//                 } else {
                     
-                    dp[i][j] = max(dp[i-1][j], dp[i][j-1]);
+//                     dp[i][j] = max(dp[i-1][j], dp[i][j-1]);
                     
-                }
+//                 }
                 
+//             }
+//         }
+        
+//         int lcs = dp[word1Len][word2Len];
+//         return word1Len + word2Len - 2 * lcs;
+        
+        int word1Len = size(word1);
+        int word2Len = size(word2);
+        
+        vector<int> current(word2Len + 1, 0);
+        vector<int> previous(word2Len + 1, 0);
+        
+        for (int i = 1; i <= word1Len; i++) {
+            
+            for (int j = 1; j <= word2Len; j++) {
+                                    
+                current[j] = word1[i-1] == word2[j-1] 
+                    ? 
+                    previous[j-1]+ 1 
+                    : max(current[j-1], previous[j]);
             }
+            previous = current;
         }
         
-        int lcs = dp[word1Len][word2Len];
+        int lcs = current[word2Len];
         return word1Len + word2Len - 2 * lcs;
     }
 };
