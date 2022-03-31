aee08d1f3d0b7b07adb88599d120ba3230ed0602
MuhtasimTanmoy <taaanmo0oy@gmail.com>
2022-03-31 10:21:47 +0600

    Update climbing stairs

--- leetcode/climbing-stairs/climbing-stairs.cpp
+++ leetcode/climbing-stairs/climbing-stairs.cpp
@@ -18,3 +18,30 @@ public:
         return bag[n];
     }
 };
+
+//--- method 1: dp iteration
+class Solution {
+public:
+    int climbStairs(int n) {
+        vector<int> dp(n+1, 0);
+        dp[0] = dp[1] = 1;
+        for (int i = 2; i <= n; ++i) {
+            dp[i] = dp[i-1]+dp[i-2];
+        }
+        return dp.back();
+    }
+};
+
+//--- method 2: O(1) space
+class Solution {
+public:
+    int climbStairs(int n) {
+        int last1 = 1, last2 = 1;
+        for (int i = 2; i <= n; ++i) {
+            auto tmp = last1+last2;
+            last2 = last1;
+            last1 = tmp;
+        }
+        return last1;
+    }
+};
