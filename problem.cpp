08e67bc1ec5db74a83318883e58597c73c9b62c8
MuhtasimTanmoy <taaanmo0oy@gmail.com>
2022-04-02 10:06:39 +0600

    Debug update

--- problem.cpp
+++ problem.cpp
@@ -2,8 +2,17 @@
 using namespace std;
 
 #define log(x) cout << #x << " = " << x << endl;
+void debug_out() { cerr << endl; }
+template <typename Head, typename... Tail>
+void debug_out(Head H, Tail... T) {
+  cerr << " " << to_string(H);
+  debug_out(T...);
+}
+#define debug(...) cerr << "Line : " << __LINE__ <<" [" << #__VA_ARGS__ << "]:", debug_out(__VA_ARGS__)
 
-void execute() {}
+void execute() {
+
+}
 
 int main()
 {
