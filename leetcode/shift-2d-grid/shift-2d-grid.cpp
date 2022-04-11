9827c0b36c39f10e3dd7ec4068fcabab842bd17b
Muhtasim Ulfat tanmoy <mtanmoy5086@gmail.com>
2022-04-11 17:11:17 +0600

    Time: 32 ms (71.23%), Space: 13.9 MB (73.22%) - LeetHub

---
+++ leetcode/shift-2d-grid/shift-2d-grid.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    vector<vector<int>> shiftGrid(vector<vector<int>>& grid, int k) {
+        
+        int n=grid.size();
+        int m=grid[0].size();
+        
+        vector<vector<int>> ans(n,vector<int>(m));
+        
+        for (int i=0;i<n;i++) {
+            
+            for (int j = 0; j < m; j++) {
+                
+                int newJ = ( j + k ) % m; // (j + numbers of columns added)%m
+                
+                int newI = ( i + ( j + k ) / m ) % n; // (i + numbers of rows added)%n 
+                
+                ans[newI][newJ]=grid[i][j];
+            }
+        }
+        return ans;
+    }
+};
