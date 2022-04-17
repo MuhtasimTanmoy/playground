0b0d3b6de6abba7c1c8484581c26b44c33d188e1
MuhtasimTanmoy <taaanmo0oy@gmail.com>
2022-04-18 02:37:41 +0600

    Update

--- problem.cpp
+++ problem.cpp
@@ -1,16 +1,33 @@
+// Find K-Length Substrings With No Repeated Characters
+
 #include <bits/stdc++.h>
 using namespace std;
 
 #define log(x) cout << #x << " = " << x << endl;
-void debug_out() { cerr << endl; }
-template <typename Head, typename... Tail>
-void debug_out(Head H, Tail... T) {
-  cerr << " " << to_string(H);
-  debug_out(T...);
-}
 
-#define debug(...) cerr << "Line : " << __LINE__ <<" [" << #__VA_ARGS__ << "]:", debug_out(__VA_ARGS__)
-void execute() {}
+void execute() {
+
+	string s;
+	cin >> s;
+
+	int k;
+	cin >> k;
+
+	int characterMap[26];
+
+	int left = 0;
+	int right = 0;
+	
+	while ( right < size(s) ) {
+		
+		while(characterMap[s[right]]) {
+			characterMap[left]--;
+			left++;
+		}
+
+		right
+	}
+}
 
 int main() {
 	ios_base::sync_with_stdio(false);
@@ -18,8 +35,6 @@ int main() {
 	freopen("input.txt", "r", stdin);
     int no_of_test_cases = 1;
 	// cin >> no_of_test_cases;
-    while (no_of_test_cases--) {
-		execute(); 
-	} 
+    while (no_of_test_cases--) execute(); 
 	return 0;
 }
