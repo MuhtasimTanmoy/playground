5608488194f95d1c91b4bb70baf505be56b919b8
MuhtasimTanmoy <taaanmo0oy@gmail.com>
2022-04-18 03:59:03 +0600

    battleships-in-a-board.cpp update

--- leetcode/battleships-in-a-board/battleships-in-a-board.cpp
+++ leetcode/battleships-in-a-board/battleships-in-a-board.cpp
@@ -37,3 +37,20 @@ public:
         return countBattleShip;
     }
 };
+
+
+// o(n)
+class Solution {
+public:
+    int countBattleships(vector<vector<char>>& board) {
+        int res = 0, row = board.size(), col = board[0].size();
+        for (int i = 0; i < row; ++i) {
+            for (int j = 0; j < col; ++j) {
+                if (board[i][j] == 'X' && (i+1 == row || board[i+1][j] == '.') && (j+1 == col || board[i][j+1] == '.')) {
+                    ++res;
+                }
+            }
+        }
+        return res;
+    }
+};
