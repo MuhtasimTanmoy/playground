5650645c41859e1085fa4cdc4353c2133a846b43
MuhtasimTanmoy <taaanmo0oy@gmail.com>
2022-04-18 02:56:43 +0600

    update all-elements-in-two-binary-search-trees

--- leetcode/all-elements-in-two-binary-search-trees/all-elements-in-two-binary-search-trees.cpp
+++ leetcode/all-elements-in-two-binary-search-trees/all-elements-in-two-binary-search-trees.cpp
@@ -38,3 +38,37 @@ public:
         return result;
     }
 };
+
+// o(n)
+class Solution2 {
+public:
+    vector<int> getAllElements(TreeNode* root1, TreeNode* root2) {
+        vector<TreeNode *> sk1, sk2;
+        vector<int> res;
+        while (1) {
+            while (root1) {
+                sk1.push_back(root1);
+                root1 = root1->left;
+            }
+            while (root2) {
+                sk2.push_back(root2);
+                root2 = root2->left;
+            }
+            if (!sk1.size() && !sk2.size()) {
+                break;
+            }
+            TreeNode *now1 = sk1.size() ? sk1.back() : NULL;
+            TreeNode *now2 = sk2.size() ? sk2.back() : NULL;
+            if (!now1 || now1 && now2 && now1->val > now2->val) {
+                res.push_back(now2->val);
+                sk2.pop_back();
+                root2 = now2->right;
+            } else if (!now2 || now1 && now2 && now1->val <= now2->val) {
+                res.push_back(now1->val);
+                sk1.pop_back();
+                root1 = now1->right;
+            }
+        }
+        return res;
+    }
+};
