ef28f5c6ba7ff6d8b399e1f4b1c26fc15093a159
Muhtasim Ulfat tanmoy <mtanmoy5086@gmail.com>
2022-04-18 19:31:54 +0600

    Time: 55 ms (63.03%), Space: 27.6 MB (97.90%) - LeetHub

--- leetcode/maximum-product-of-three-numbers/maximum-product-of-three-numbers.cpp
+++ leetcode/maximum-product-of-three-numbers/maximum-product-of-three-numbers.cpp
@@ -3,12 +3,10 @@ public:
     int maximumProduct(vector<int>& nums) {
         sort(begin(nums), end(nums));
 
-        return max(
-            nums[0] * nums[1] * nums[2], 
+        return 
             max(
                 nums[0] * nums[1] * nums[size(nums)-1],
                 nums[size(nums)-1] * nums[size(nums)-2] * nums[size(nums)-3]
-            )
-        );
+            );
     }
 };
