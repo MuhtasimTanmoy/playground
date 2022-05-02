5c1494a38866c28cbcd551785e515ec2d1411e6e
Muhtasim Ulfat tanmoy <mtanmoy5086@gmail.com>
2022-05-02 16:04:15 +0600

    Time: 3 ms (99.44%), Space: 16.3 MB (57.28%) - LeetHub

--- leetcode/sort-array-by-parity/sort-array-by-parity.cpp
+++ leetcode/sort-array-by-parity/sort-array-by-parity.cpp
@@ -1,14 +1,39 @@
 class Solution {
 public:
+    
+//     new array
+//
+//     vector<int> sortArrayByParity(vector<int>& nums) {
+//         int n = size(nums);
+//         int start = 0 , end = n - 1;\
+//         vector<int> ans(n);
+//         for(auto x : nums) {
+//             if(x & 1 == 0) ans[start++] = x;
+//             else ans[end--] = x;
+//         }
+//         return ans ;
+//     }
+    
+    void swap(int &a, int& b) {
+        a = a ^ b;
+        b = a ^ b;
+        a = a ^ b;
+    }
+    
+    // in place
     vector<int> sortArrayByParity(vector<int>& nums) {
-        int n = size(nums);
-        int start = 0 , end = n - 1;
-        vector<int> ans(n);
         
-        for(auto x : nums) {
-            if(x % 2 == 0) ans[start ++] = x;
-            else ans[end--] = x;
+        int length = size(nums);
+        int start = 0;
+        
+        for( int i = 0; i < length; i++ ) {
+            if ( ( nums[i] & 1 ) == 0) {
+                if ( i != start) swap(nums[i], nums[start]);
+                start++;
+            }
         }
-        return ans ;
+        
+        return nums;
+        
     }
 };
