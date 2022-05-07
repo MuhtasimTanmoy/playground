441b31b391072290fba8c9e3ff6e6c3000eb02d8
MuhtasimTanmoy <taaanmo0oy@gmail.com>
2022-05-07 20:42:02 +0600

    Add string optimization

--- Algorithms/union-find.cpp
+++ Algorithms/union-find.cpp
@@ -31,18 +31,44 @@ public:
             root[i] = i;
     }
 
+
+    // normal
     int find(int x) {
         while (x != root[x]) 
             x = root[x];
         return x;
     }
 
+    // path compression
+    int find(int x) {
+        if (x == root[x]) 
+            return x;
+        return root[x] = find(root[x]);
+    }
+
+    // normal
     void unionSet(int x, int y) {
         int rootX = find(x);
         int rootY = find(y);
-        if (rootX != rootY) 
-            root[rootY] = rootX;
+        if (rootX != rootY) root[rootY] = rootX;
     }
+
+    // rank based
+    void unionSetRankBased(int x, int y) {
+        int rootX = find(x);
+        int rootY = find(y);
+        if (rootX != rootY) {
+            if (rank[rootX] > rank[rootY])
+                root[rootY] = rootX;
+            else if (rank[rootX] < rank[rootY])
+                root[rootX] = rootY;
+            else {
+                root[rootY] = rootX;
+                rank[rootX] += 1;
+            }
+        }
+    }
+
 
     bool connected(int x, int y) {
         return find(x) == find(y);
