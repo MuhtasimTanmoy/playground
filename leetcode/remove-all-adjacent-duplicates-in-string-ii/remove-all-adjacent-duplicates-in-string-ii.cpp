dcac3de9455c93ca3a74581d8edd7eca4d14f30b
Muhtasim Ulfat tanmoy <mtanmoy5086@gmail.com>
2022-05-07 22:06:21 +0600

    Time: 24 ms (71.52%), Space: 10.8 MB (44.65%) - LeetHub

--- leetcode/remove-all-adjacent-duplicates-in-string-ii/remove-all-adjacent-duplicates-in-string-ii.cpp
+++ leetcode/remove-all-adjacent-duplicates-in-string-ii/remove-all-adjacent-duplicates-in-string-ii.cpp
@@ -1,35 +1,17 @@
 class Solution {
 public:
     string removeDuplicates(string s, int k) {
-        
-        stack<pair<char, int>> stack;
-        int sameCharCount = 0;
-        
-        for(auto c: s) {
-            if (stack.empty()) stack.push({c, 1});
-            else {
-                auto topElem = stack.top();
-                if (topElem.first != c) stack.push( {c, 1} );
-                else {
-                    if (topElem.second == k - 1) stack.pop();
-                    else {
-                        stack.pop();
-                        stack.push(
-                            {topElem.first, 
-                             topElem.second + 1}
-                        );
-                    }
-                }
-            }
+        vector<pair<char, int>> sk;
+        string res;
+        for (int i = 0; i < s.size(); ++i) {
+            if (sk.size() && sk.back().first == s[i]) {
+                if (++sk.back().second == k) sk.pop_back();
+            } else 
+                sk.push_back({s[i], 1});
         }
-        
-        string result = "";
-        while (!stack.empty()) {
-            while(stack.top().second--) result += stack.top().first;
-            
-            stack.pop();
-        }
-        reverse(result.begin(), result.end());
-        return result;
+        for (int i = 0; i < sk.size(); ++i) 
+            for (int j = 0; j < sk[i].second; ++j) 
+                res.push_back(sk[i].first);
+        return res;
     }
 };
