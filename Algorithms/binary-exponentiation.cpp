fce1723d98f15badadd64c941a757050da58a53f
MuhtasimTanmoy <taaanmo0oy@gmail.com>
2022-05-08 00:00:15 +0600

    binary exponentiation add

---
+++ Algorithms/binary-exponentiation.cpp
@@ -0,0 +1,10 @@
+long long binpow(long long a, long long b) {
+    long long res = 1;
+    while (b > 0) {
+        if (b & 1)
+            res = res * a;
+        a = a * a;
+        b >>= 1;
+    }
+    return res;
+}
