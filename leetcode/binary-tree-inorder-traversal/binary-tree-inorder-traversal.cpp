42419627736c171c0ee76d3324826cc0fbccd78d
Muhtasim Ulfat tanmoy <mtanmoy5086@gmail.com>
2022-05-09 04:46:34 +0600

    Time: 8 ms (7.31%), Space: 8.2 MB (92.32%) - LeetHub

--- leetcode/binary-tree-inorder-traversal/binary-tree-inorder-traversal.cpp
+++ leetcode/binary-tree-inorder-traversal/binary-tree-inorder-traversal.cpp
@@ -10,17 +10,38 @@
  * };
  */
 class Solution {
-public:
-    vector<int> result;
-    void inorder(TreeNode* root) {
-        if ( root == nullptr ) { return; }
-        inorder(root->left);
-        result.push_back(root->val);
-        inorder(root->right);
+    
+    vector<int> morrisTraversal(TreeNode* root) {
+        
+        vector<int> result;
+        TreeNode* current = root;
+        
+        while (current) {
+            
+            if (current->left == nullptr) {
+                result.push_back(current->val);
+                current = current->right;
+            } else {
+                TreeNode* predecessor = current->left;
+                
+                while (predecessor->right != current && predecessor->right) 
+                    predecessor = predecessor->right;
+                
+                if (predecessor->right) {
+                    predecessor->right = nullptr;
+                    result.push_back(current->val);
+                    current = current->right;
+                } else {
+                    predecessor->right = current;
+                    current = current->left;
+                }
+            } 
+        }
+        return result;
     }
     
+public:
     vector<int> inorderTraversal(TreeNode* root) {
-        inorder(root);
-        return result;
+        return morrisTraversal(root);
     }
 };
