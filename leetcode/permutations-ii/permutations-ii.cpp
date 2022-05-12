60efe1380ff8e5c640bb50beafeeaf804bb535c6
Muhtasim Ulfat tanmoy <mtanmoy5086@gmail.com>
2022-05-12 07:42:28 +0600

    Time: 16 ms (44.91%), Space: 8.4 MB (95.68%) - LeetHub

--- leetcode/permutations-ii/permutations-ii.cpp
+++ leetcode/permutations-ii/permutations-ii.cpp
@@ -1,14 +1,23 @@
 class Solution {
+    bool nextPermutation(vector<int>& s) {
+        int i = size(s) - 1;
+        if (!i) return false;
+        while (s[i-1] >= s[i]) if (--i == 0) return false;
+
+        int j = size(s) - 1;
+        while (j > i && s[j] <= s[i-1]) j--;
+
+        std::swap(s[i-1], s[j]);
+        std::reverse (s.begin() + i, s.end());
+        return true;
+    }
 public:
     vector<vector<int>> permuteUnique(vector<int>& nums) {
         sort(begin(nums), end(nums));
-        
         vector<vector<int>> result;
-
         do {
             result.push_back(nums);
-        } while (next_permutation(begin(nums), end(nums)));
-
+        } while (nextPermutation(nums));
         return result;
     }
 };
