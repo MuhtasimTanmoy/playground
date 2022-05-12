c2559a5f2fb8e1f1d04b6bdaa0363ef4a14a514e
MuhtasimTanmoy <taaanmo0oy@gmail.com>
2022-05-12 07:45:07 +0600

    Permutation add

--- Algorithms/permutation.cpp
+++ Algorithms/permutation.cpp
@@ -10,39 +10,54 @@ using namespace std;
 
 const int N = 2e5 + 11;
 
+//  vector<vector<int>> permuteUnique(vector<int>& nums) {
+//         sort(begin(nums), end(nums));
+//         vector<vector<int>> result;
+//         do {
+//             result.push_back(nums);
+//         } while (nextPermutation(nums));
+//         return result;
+//     }
+
+bool nextPermutation(vector<int>& s) {
+	
+	int i = size(s) - 1;
+	if (!i) return false;
+	
+	while (s[i-1] >= s[i]) if (--i == 0) return false;
+
+	int j = size(s) - 1;
+	while (j > i && s[j] <= s[i-1]) j--;
+
+	std::swap(s[i-1], s[j]);
+	std::reverse (s.begin() + i, s.end());
+	return true;
+}
 
 // BUILT IN
-void solve()
-{
+void solve() {
 	int n;
 	cin >> n;
 	int li[n];
-	for (int i = 0; i < n; i++) {
+	for (int i = 0; i < n; i++) 
 		cin >> li[i];
-	}
 
 	do {
-		for (auto x : li) {
+		for (auto x : li) 
 			cout << x << " ";
-		}
 		cout << endl;
 	} while (next_permutation(li, li + n));
 }
 
-int main()
-{
+int main() {
 	freopen("input.txt", "r", stdin);
 	ios_base::sync_with_stdio(0);
 	cin.tie(0);
 	cout.tie(0);
-	int no_of_test_cases = 0;
+	int no_of_test_cases = 1;
 	// cin >> no_of_test_cases;
-	if (!no_of_test_cases)
-		no_of_test_cases = 1;
-	while (no_of_test_cases--)
-	{
+	while (no_of_test_cases--) 
 		solve();
-	}
 
 	return 0;
 }
