198ea61d54780be50d7515de9b8abedb9cafd75c
Muhtasim Ulfat tanmoy <mtanmoy5086@gmail.com>
2022-05-19 01:33:48 +0600

    Time: 86 ms (44.25%), Space: 21.8 MB (58.37%) - LeetHub

--- leetcode/number-of-enclaves/number-of-enclaves.cpp
+++ leetcode/number-of-enclaves/number-of-enclaves.cpp
@@ -1,41 +1,51 @@
 class Solution {
 public:
-    
-    void mark(vector<vector<int>>& grid, int i, int j) {
-
-        if (!( i >= 0 && i < size(grid) )) { return; }
-        if (!( j >= 0 && j < size(grid[0]) )) { return; }
-        
-        if (grid[i][j] == 0 ) { return; }
-        grid[i][j] = 0;
-            
-        mark(grid, i+1, j);
-        mark(grid, i, j+1);
-        mark(grid, i-1, j);
-        mark(grid, i, j-1);
-    }
-    
     int numEnclaves(vector<vector<int>>& grid) {
-        
-        for(int i = 0; i < size(grid); i++ ) {
-            for(int j = 0; j < size(grid[0]); j++ ) {
-                if (i == 0 
-                    || j == 0 
-                    || i == size(grid) - 1 
-                    || j == size(grid[0]) - 1) {
-                    
-                    cout<<"marking"<<i<<" "<<j<<endl;
-                    if (grid[i][j] == 1) mark(grid, i, j);
-                }
-            }
-        }
-        
         int count = 0;
-        for(int i = 0; i < size(grid); i++ ) {
-            for(int j = 0; j < size(grid[0]); j++ ) {
-                if (grid[i][j] == 1) count++;
+         vector<pair<int, int>> directions = { {0, 1}, {0, -1}, {-1, 0}, {1, 0} };
+        
+        function<void (int, int)> markDead = [&] (int x, int y) {
+            grid[x][y] = 0;
+            for (auto direction: directions) {
+                int nx = x + direction.first;
+                int ny = y + direction.second;
+                if (nx < 0 
+                    || ny < 0 
+                    || nx >= grid.size() 
+                    || ny >= grid[0].size() 
+                    || !grid[nx][ny]) continue;
+                markDead(nx, ny);
             }
-        }
+        };
+        
+        for(int i = 0; i < grid.size(); i++)
+            for(int j = 0; j < grid[i].size(); j++)
+                if (i == 0 
+                    || j == 0
+                    || i == grid.size() - 1 
+                    || j == grid[0].size() - 1) {
+                    if (grid[i][j]) markDead(i, j);
+            }
+        
+         function<void (int, int)> traverseMark = [&] (int x, int y) {
+            grid[x][y] = 0;
+            count++;
+            for (auto direction: directions) {
+                int nx = x + direction.first;
+                int ny = y + direction.second;
+                if (nx < 0 
+                    || ny < 0 
+                    || nx >= grid.size() 
+                    || ny >= grid[0].size() 
+                    || !grid[nx][ny]) continue;
+                traverseMark(nx, ny);
+            }
+        };
+        
+        for(int i = 0; i < grid.size(); i++)
+            for(int j = 0; j < grid[i].size(); j++) 
+                if (grid[i][j]) 
+                    traverseMark(i, j);
         
         return count;
     }
