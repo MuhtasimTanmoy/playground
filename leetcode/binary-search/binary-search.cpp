5237d182e1117d07c1e0098b39bed309f46b6c51
Muhtasim Ulfat tanmoy <mtanmoy5086@gmail.com>
2022-05-19 13:43:31 +0600

    Time: 56 ms (35.22%), Space: 27.6 MB (12.95%) - LeetHub

---
+++ leetcode/binary-search/binary-search.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    int search(vector<int>& nums, int target) {
+        int left = 0;
+        int right = size(nums) - 1;
+        
+        while(left <= right) {
+            int mid = left + ( (right - left) >> 1);
+            if (nums[mid] == target) return mid;
+            else if (nums[mid] > target) { right = mid - 1;}
+            else { left = mid + 1;}
+        }
+        return -1;
+    }
+};
