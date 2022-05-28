292dd34da94ab23305a5e8da1b2c1aeccbeeb51e
Muhtasim Ulfat tanmoy <mtanmoy5086@gmail.com>
2022-05-28 11:37:35 +0600

    Time: 33 ms (30.82%), Space: 18 MB (65.37%) - LeetHub

--- leetcode/missing-number/missing-number.cpp
+++ leetcode/missing-number/missing-number.cpp
@@ -1,13 +1,11 @@
 class Solution {
 public:
     int missingNumber(vector<int>& nums) {
-        
         int sum = 0;
-        for(int i = 0; i < nums.size(); i++) {
-            sum += i + 1 ;
-            sum -= nums[i];
+        for (int i = 0; i < size(nums); i++) {
+            sum ^= i + 1;
+            sum ^= nums[i];
         }
-        
         return sum;
     }
 };
