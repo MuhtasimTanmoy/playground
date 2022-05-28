bf2052a2b96aed6789c9832860d0edffb89ac27d
Muhtasim Ulfat tanmoy <mtanmoy5086@gmail.com>
2022-05-28 12:44:02 +0600

    Time: 6 ms (50.89%), Space: 10.1 MB (92.77%) - LeetHub

--- leetcode/find-unique-binary-string/find-unique-binary-string.cpp
+++ leetcode/find-unique-binary-string/find-unique-binary-string.cpp
@@ -1,15 +1,8 @@
 class Solution {
 public:
     string findDifferentBinaryString(vector<string>& nums) {
-        
-        int n = size(nums);
-        
-        string res;
-        res.resize(n);
-        
-        for(int i = 0; i < size(nums); i++)
-            res[i] = nums[i][i] == '0' ? '1' : '0';
-        
+        string res = nums[0];
+        for(int i = 0; i < size(nums); i++) res[i] = nums[i][i] == '0' ? '1' : '0';
         return res;
     }
 };
