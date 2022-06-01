81733a28ad20ea192235c5f8a34cd5cafe2e69ee
Muhtasim Ulfat tanmoy <mtanmoy5086@gmail.com>
2022-06-02 00:08:34 +0600

    Time: 4 ms (36.66%), Space: 5.9 MB (25.41%) - LeetHub

--- leetcode/power-of-two/power-of-two.cpp
+++ leetcode/power-of-two/power-of-two.cpp
@@ -1,18 +1,12 @@
 class Solution {
 public:
     bool isPowerOfTwo(int n) {
+        if (!n) return false;
         
-        int oneCount = 0;
-        if( n < 0) { return false; }
-        
-        for(int i = 0; i < 32; i++) {
-            
-            if ( n & (1 << i) ) {
-                oneCount++;
-            }
-            
+        while(n != 1) {
+            if (n % 2) return false;
+            n = n >> 1;
         }
-        
-        return oneCount == 1;
+        return true;
     }
 };
