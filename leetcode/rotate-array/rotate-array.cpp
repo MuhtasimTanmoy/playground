49d4451bff0bd57e617ee5ed883d430709c8da3f
Muhtasim Ulfat tanmoy <mtanmoy5086@gmail.com>
2022-06-01 19:33:26 +0600

    Time: 37 ms (65.39%), Space: 25.6 MB (24.98%) - LeetHub

--- leetcode/rotate-array/rotate-array.cpp
+++ leetcode/rotate-array/rotate-array.cpp
@@ -1,16 +1,13 @@
 class Solution {
 public:
-    void rotate(vector<int>& nums, int k) {
-        
-        vector<int> cache = nums;
-        auto swapWithOffset = [&](int first) {
-            int num = cache[first];
-            int replaceToIndex = (first + k ) % nums.size();
-            nums[ replaceToIndex ] = num;
-        };
-        
-        for( int i = 0; i< nums.size(); i++) {
-            swapWithOffset(i);
+    void rotate(vector<int>& nums, int k) {   
+        vector<int> result(size(nums));
+        for (int i = 0; i < size(nums); i++) {
+            int target = ( i + k ) % size(nums);
+            result[target] = nums[i];
         }
+        
+        for (int i = 0; i < size(nums); i++)
+            nums[i] = result[i];
     }
 };
