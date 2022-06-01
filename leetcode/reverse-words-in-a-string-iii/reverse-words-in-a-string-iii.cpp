68462f1248546e160fa0081ee1233d34124fe263
Muhtasim Ulfat tanmoy <mtanmoy5086@gmail.com>
2022-06-01 15:49:32 +0600

    Time: 18 ms (57.85%), Space: 9.5 MB (99.02%) - LeetHub

--- leetcode/reverse-words-in-a-string-iii/reverse-words-in-a-string-iii.cpp
+++ leetcode/reverse-words-in-a-string-iii/reverse-words-in-a-string-iii.cpp
@@ -1,52 +1,28 @@
 class Solution {
-    
-    typedef struct Span {
-        int start;
-        int end;
-        
-        Span(int start, int end)
-        : start(start)
-        ,end(end)
-        {}
-        
-    } Span; 
-    
 public:
     string reverseWords(string s) {
         
-        vector<Span> spaceOccurence;
-        int traverse = 0;
-        
-        int start = 0;
-        int end = size(s) - 1;
-        
-        for(int i = 0; i <= size(s); i++) {
-            if(s[i] == ' ' || i ==  size(s)) {
-                spaceOccurence.emplace_back(start, i-1);
-                start = i + 1;
-                traverse++;
-            }
-        }
-        
-        auto swap = [](char& a, char& b) {
-            a = a ^ b;
-            b = a ^ b;
-            a = a ^ b;
-        };
-        
-        auto reverse = [&](int start, int end) {
-            while(start < end) {
-                swap(s[start], s[end]);
-                start++;
-                end--;
+        auto reverse = [&](int left, int right) {
+            while(left < right) {
+                auto c = s[left];
+                s[left] = s[right];
+                s[right] = c;
+                left++;
+                right--;
             }
         };
         
-        for(int word = 0; word < size(spaceOccurence); word++) {
-            reverse(spaceOccurence[word].start, spaceOccurence[word].end);
+        
+        int tokenBegin = 0;
+        int tokenEnd = 0;
+        
+        while(tokenBegin < size(s)) {
+            while(tokenEnd < size(s) && s[tokenEnd] != ' ') tokenEnd++;
+            reverse(tokenBegin, tokenEnd - 1);
+            tokenEnd++;
+            tokenBegin = tokenEnd;
         }
         
         return s;
-        
     }
 };
