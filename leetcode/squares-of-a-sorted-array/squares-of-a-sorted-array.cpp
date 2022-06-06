80757b8abd10b976894371ea598282864c08e611
Muhtasim Ulfat tanmoy <mtanmoy5086@gmail.com>
2022-06-07 04:26:19 +0600

    Time: 25 ms (97.69%), Space: 25.9 MB (55.76%) - LeetHub

--- leetcode/squares-of-a-sorted-array/squares-of-a-sorted-array.cpp
+++ leetcode/squares-of-a-sorted-array/squares-of-a-sorted-array.cpp
@@ -1,8 +1,34 @@
+// class Solution {
+// public:
+//     vector<int> sortedSquares(vector<int>& nums) {
+//         for(auto &num: nums) num = num * num;
+//         sort(nums.begin(), nums.end());
+//         return nums;
+//     }
+// };
+
 class Solution {
 public:
     vector<int> sortedSquares(vector<int>& nums) {
-        for(auto &num: nums) num = num * num;
-        sort(nums.begin(), nums.end());
-        return nums;
+        int left = 0;
+        int right = size(nums) - 1;
+        
+        int traverse = right;
+        vector<int> result(size(nums));
+        
+        while(traverse >= 0) {
+            int leftElem = abs(nums[left]);
+            int rightElem = abs(nums[right]);
+            
+            if (leftElem > rightElem) {
+                left++;
+                result[traverse--] = leftElem * leftElem;
+            } else {
+                right--;
+                result[traverse--] = rightElem * rightElem;
+            }
+        }
+        
+        return result;
     }
 };
