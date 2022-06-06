62ab5709450cbded88baa540097325899e90295f
Muhtasim Ulfat tanmoy <mtanmoy5086@gmail.com>
2022-06-07 04:26:39 +0600

    Time: 28 ms (70.36%), Space: 19.3 MB (14.37%) - LeetHub

--- leetcode/move-zeroes/move-zeroes.cpp
+++ leetcode/move-zeroes/move-zeroes.cpp
@@ -2,9 +2,7 @@ class Solution {
 public:
     void moveZeroes(vector<int>& nums) {
         int left = 0;
-        for(int i = 0; i < size(nums); i++)
-            if (nums[i]) nums[left++] = nums[i];
-        
+        for(int i = 0; i < size(nums); i++) if (nums[i]) nums[left++] = nums[i];
         while(left < size(nums)) nums[left++] = 0;
     }
 };
