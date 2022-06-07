2cc84856d4be4dfc9971bc125609b98a5399c579
MuhtasimTanmoy <taaanmo0oy@gmail.com>
2022-06-08 04:55:26 +0600

    Update struture practice

--- practice/Interview/replace-every-elem-with-greatest-right.cpp
+++ practice/Interview/replace-every-elem-with-greatest-right.cpp
@@ -5,60 +5,44 @@ using namespace std;
 const int MAX = 1e7 + 1;
 long long li[MAX];
 
-void solve()
-{
+void solve() {
 	int n;
 	cin >> n;
 
 	for (int i = 0; i < n; i++)
-	{
 		cin >> li[i];
-	}
 
 	int start = 0;
 	int end = n - 1;
 
 	int diff = -1;
 
-	while (start < end)
-	{
-		if (li[start] <= li[end])
-		{
+	while (start < end) {
+		if (li[start] <= li[end]) {
 			diff = end - start;
 			break;
-		}
-		else
-		{
-			if ((li[start] - li[start + 1]) > (li[end - 1] - li[end]))
-			{
-				start++;
-			}
-			else
-			{
-				end--;
-			}
+		} else {
+			if ((li[start] - li[start + 1]) > (li[end - 1] - li[end])) start++;
+			else end--;
 		}
 	}
 
 	cout << diff << endl;
 }
 
-void solve_geek_for_geeks()
-{
+void solve_geek_for_geeks() {
 	// https://www.geeksforgeeks.org/given-an-array-arr-find-the-maximum-j-i-such-that-arrj-arri/
 }
 
-void solve2()
-{
-	// https://www.geeksforgeeks.org/replace-every-element-with-the-greatest-on-right-side/
+// https://www.geeksforgeeks.org/replace-every-element-with-the-greatest-on-right-side/
+void solve2() {
 	int n;
 	cin >> n;
 
 	int holder;
 	vector<int> li(n);
 
-	for (int i = 0; i < n; i++)
-	{
+	for (int i = 0; i < n; i++) {
 		cin >> holder;
 		li[i] = holder;
 	}
@@ -66,35 +50,28 @@ void solve2()
 	int current = -1;
 	int nextMax = -1;
 
-	for (int i = n - 1; i >= 0; i--)
-	{
+	for (int i = n - 1; i >= 0; i--) {
 		current = max(nextMax, current);
 		nextMax = max(nextMax, li[i]);
 		li[i] = current;
 	}
 
 	for (int i = 0; i < n; i++)
-	{
 		cout << li[i] << " ";
-	}
 }
 
-int main()
-{
+int main() {
 #ifdef _DEBUG
 	freopen("input.txt", "r", stdin);
 #endif
 
 	ios_base::sync_with_stdio(false);
-	int no_of_test_cases;
+	int no_of_test_cases = 1;
 	// cin >> no_of_test_cases;
-	no_of_test_cases = 1;
-	while (no_of_test_cases--)
-	{
+	while (no_of_test_cases--) {
 		// solve();
 		// solve_geek_for_geeks()
 		solve2();
 	}
-
 	return 0;
 }
