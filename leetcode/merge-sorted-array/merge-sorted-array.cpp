76d356c259370a2f5c41f00bf6a0bc20245bf887
Muhtasim Ulfat tanmoy <mtanmoy5086@gmail.com>
2022-06-07 16:44:02 +0600

    Time: 8 ms (15.01%), Space: 9.1 MB (30.68%) - LeetHub

--- leetcode/merge-sorted-array/merge-sorted-array.cpp
+++ leetcode/merge-sorted-array/merge-sorted-array.cpp
@@ -1,28 +1,20 @@
 class Solution {
 public:
     void merge(vector<int>& nums1, int m, vector<int>& nums2, int n) {
-        
-        if ( m == 0 ) { 
-            nums1 = nums2;
-            return;
-        }
+        if (!m) nums1 = nums2;
         
         int num1Itr = m - 1;
-        int num2Itr = n;
+        int num2Itr = n - 1;
+        int resItr = m + n - 1;
         
-        int totalItr = m + n - 1;
+        while (num2Itr >=0 && num1Itr >= 0)
+            nums1[resItr--] = 
+            (nums1[num1Itr] > nums2[num2Itr])
+            ? nums1[num1Itr--]
+            : nums2[num2Itr--];
         
-        while(num2Itr--) {
-            
-            if ( num1Itr < 0 || nums1[num1Itr] < nums2[num2Itr] ) {
-                nums1[totalItr] = nums2[num2Itr];
-            } else {
-                nums1[totalItr] = nums1[num1Itr];
-                num1Itr--;
-                num2Itr++;
-            }
-            
-            totalItr--;
-        }
+        cout<<num2Itr<<" "<<num1Itr<<" "<<resItr<<endl;
+        while(num2Itr >= 0) nums1[resItr--] = nums2[num2Itr--];
+        // while(num1Itr >= 0) nums1[resItr--] = nums1[num2Itr--];
     }
 };
