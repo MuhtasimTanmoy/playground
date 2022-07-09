8d188d7ed0399a1551843c123d0ef16c014d7c91
MuhtasimTanmoy <taaanmo0oy@gmail.com>
2022-07-09 15:55:48 +0600

    Restructure

--- problem.cpp
+++ problem.cpp
@@ -2,12 +2,18 @@
 using namespace std;
 
 #define log(x) cout << #x << " = " << x << endl;
-void execute() {}
+void execute() {
+	cout << "Enter a string: ";
+    getline(cin, str1);
+
+    cout << "Enter another string: ";
+    cin.get(str, 100, '\n');
+}
 
 int main() {
 	ios_base::sync_with_stdio(false);
     cin.tie(0);
-	freopen("input.txt", "r", stdin);
+	// freopen("input.txt", "r", stdin);
     int no_of_test_cases = 1;
 	cin >> no_of_test_cases;
     while (no_of_test_cases--) execute(); 
