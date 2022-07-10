6975c56e6e53e07ea45c2355db2a7b17948fd1a0
MuhtasimTanmoy <taaanmo0oy@gmail.com>
2022-07-10 21:39:58 +0600

    Update KMP

--- Algorithms/kmp.cpp
+++ Algorithms/kmp.cpp
@@ -1,65 +1,44 @@
-#include <iostream>
-#include <string>
+#include <vector>
+#include<iostream>
 
 using namespace std;
 
 // AAACAAAAAC
-int *pre_kmp(string pattern) {
+vector<int> pre_kmp(string pattern) {
 	// k means the pattern in this case C the lst 3 no while dont match
 	int size = pattern.size();
-	int *pie = new int[size];
-	pie[0] = 0;
-	int k = 0;
+	vector<int> suffixPrefixLen(size, 0);
+	int upto = 0;
 	for (int i = 1; i < size; i++) {
-		while (k > 0 && pattern[k] != pattern[i]) k = pie[k - 1];
-		if (pattern[k] == pattern[i]) k = k + 1;
-		pie[i] = k;
+		while (upto > 0 && pattern[upto] != pattern[i]) upto = suffixPrefixLen[upto - 1];
+		if (pattern[upto] == pattern[i]) upto++;
+		suffixPrefixLen[i] = upto;
 	}
-	return pie;
+	return suffixPrefixLen;
 }
 
 void kmp(string text, string pattern) {
-	int *pie = pre_kmp(pattern);
-	int matched_pos = 0;
-
-	for (int current = 0; current < text.length(); current++) {
-		while (matched_pos > 0 && pattern[matched_pos] != text[current])
-			matched_pos = pie[matched_pos - 1];
-
-		if (pattern[matched_pos] == text[current])
-			matched_pos = matched_pos + 1;
-
-		if (matched_pos == (pattern.length())) {
-			cout << "Pattern occurs with shift " << current - (pattern.length() - 1);
-			matched_pos = pie[matched_pos - 1];
+	auto suffixPrefixLen = pre_kmp(pattern);
+	int upto = 0;
+	for (int i = 0; i < text.length(); i++) {
+		while (upto > 0 && pattern[upto] != text[i]) upto = suffixPrefixLen[upto - 1];
+		if (pattern[upto] == text[i]) upto++;
+		if (upto == pattern.length()) {
+			cout << "Pattern occurs with shift " << i - (pattern.length() - 1)<<"\n";
+			upto = suffixPrefixLen[upto - 1];
 		}
 	}
 }
 
-void solve() {
-	string s;
-	cin >> s;
-	int *li = pre_kmp(s);
+void printKMPArr() {
+	string s = "AAACAAAAAC";
+	auto li = pre_kmp(s);
 	int n = s.size();
 	for (int i = 0; i < n; i++) cout << li[i] << " ";
 }
 
-void solve2() {
-	string s;
-	cin >> s;
-	int kmp[s.size() + 1];
-	int prev = 0;
-	kmp[0] = -1;
-	kmp[1] = 0;
-	
-	for (int i = 1; i < s.size(); i++) {
-		if (s[prev] == s[i]) prev++;
-		else prev = 0;
-		kmp[i + 1] = prev;
-	}
-
-	for (int i = 1; i <= s.size(); i++) 
-		cout << kmp[i] << " ";
+void solve() {
+	kmp("aapatternaaaabpatterncpatpatternternpatternd", "pattern");
 }
 
 int main() {
@@ -67,10 +46,6 @@ int main() {
 	ios_base::sync_with_stdio(0);
 	cin.tie(0);
 	cout.tie(0);
-	int no_of_test_cases = 1;
-	// cin >> no_of_test_cases;
-	while (no_of_test_cases--) 
-		solve();
-		// solve2();
+	solve();
 	return 0;
 }
