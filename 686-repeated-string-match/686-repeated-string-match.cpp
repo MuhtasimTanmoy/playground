8074194cd8e2c93009295601ba46755c6e8f8322
Muhtasim Ulfat tanmoy <mtanmoy5086@gmail.com>
2022-07-12 05:05:44 +0600

    Time: 15 ms (50.58%), Space: 7 MB (82.10%) - LeetHub

---
+++ 686-repeated-string-match/686-repeated-string-match.cpp
@@ -0,0 +1,17 @@
+class Solution
+{
+    public:
+        int repeatedStringMatch(string a, string b) {
+            string s = "";
+            int count = 0;
+            while (s.size() < b.size()) {
+                s += a;
+                count++;
+            }
+            if (s.find(b) != string::npos) return count;
+            s += a;
+            count++;
+            if (s.find(b) != string::npos) return count;
+            return -1;
+        }
+};
