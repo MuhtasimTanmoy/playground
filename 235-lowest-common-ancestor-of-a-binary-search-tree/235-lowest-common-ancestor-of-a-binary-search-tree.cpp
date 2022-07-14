911892d132d7c743214743d431b6b080b56d8f73
Muhtasim Ulfat tanmoy <mtanmoy5086@gmail.com>
2022-07-15 03:24:30 +0600

    Time: 43 ms (57.41%), Space: 23.3 MB (63.67%) - LeetHub

--- 235-lowest-common-ancestor-of-a-binary-search-tree/235-lowest-common-ancestor-of-a-binary-search-tree.cpp
+++ 235-lowest-common-ancestor-of-a-binary-search-tree/235-lowest-common-ancestor-of-a-binary-search-tree.cpp
@@ -8,20 +8,46 @@
  * };
  */
 
+// class Solution {
+//     TreeNode* traverse(TreeNode* root, TreeNode* p, TreeNode* q) {
+//         if (!root) return NULL;
+//         if (root == p || root == q) return root;
+//         auto foundFromLeft = traverse(root->left, p, q);
+//         auto foundFromRight = traverse(root->right, p, q);
+//         if (foundFromLeft && foundFromRight) return root;
+//         if (foundFromLeft || foundFromRight) if (root == p || root == q) return root;
+//         if (foundFromLeft) return foundFromLeft;
+//         if (foundFromRight) return foundFromRight;
+//         return NULL;
+//     }
+// public:
+//     TreeNode* lowestCommonAncestor(TreeNode* root, TreeNode* p, TreeNode* q) {
+//         return traverse(root, p, q);
+//     }
+// };
+
 class Solution {
-    TreeNode* traverse(TreeNode* root, TreeNode* p, TreeNode* q) {
-        if (!root) return NULL;
-        if (root == p || root == q) return root;
-        auto foundFromLeft = traverse(root->left, p, q);
-        auto foundFromRight = traverse(root->right, p, q);
-        if (foundFromLeft && foundFromRight) return root;
-        if (foundFromLeft || foundFromRight) if (root == p || root == q) return root;
-        if (foundFromLeft) return foundFromLeft;
-        if (foundFromRight) return foundFromRight;
-        return NULL;
-    }
 public:
     TreeNode* lowestCommonAncestor(TreeNode* root, TreeNode* p, TreeNode* q) {
-        return traverse(root, p, q);
+        TreeNode* res = root;
+        while(true) {
+            if (res->val > p->val && res->val > q->val) res = res->left;
+            else if (res->val < p->val && res->val < q->val) res = res->right;
+            else break;
+        }
+        return res;
     }
 };
+
+// class Solution {
+// public:
+//     TreeNode* lcs(TreeNode* root, TreeNode* p, TreeNode* q) {
+//         if (root->val > p->val && root->val > q->val) { return lcs(root->left, p, q); }
+//         else if (root->val < p->val && root->val < q->val) { return lcs(root->right, p, q); }
+//         else { return root; }
+//     }
+    
+//     TreeNode* lowestCommonAncestor(TreeNode* root, TreeNode* p, TreeNode* q) {
+//         return lcs(root, p, q);
+//     }
+// };
