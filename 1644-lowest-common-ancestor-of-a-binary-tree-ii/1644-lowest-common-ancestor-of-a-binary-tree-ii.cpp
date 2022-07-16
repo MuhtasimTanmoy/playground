c4a26040c9e1d4f7fcc016fca43b19fe9725fa52
Muhtasim Ulfat tanmoy <mtanmoy5086@gmail.com>
2022-07-16 21:47:06 +0600

    Time: 151 ms (85.37%), Space: 61.7 MB (56.91%) - LeetHub

--- 1644-lowest-common-ancestor-of-a-binary-tree-ii/1644-lowest-common-ancestor-of-a-binary-tree-ii.cpp
+++ 1644-lowest-common-ancestor-of-a-binary-tree-ii/1644-lowest-common-ancestor-of-a-binary-tree-ii.cpp
@@ -9,24 +9,21 @@
  */
 class Solution {
     int count = 0;
+    
     TreeNode* ancestorIfFound(TreeNode* root, TreeNode* p, TreeNode* q) {
         if (!root) return NULL;
         auto left = ancestorIfFound(root->left, p, q);
         auto right = ancestorIfFound(root->right, p, q);
-        
         if (root == p || root == q) {
-            cout<<root->val<<endl;
             count++;
             return root;
         }
-        
         if (left && right) return root;
         else return left ? left : right;
     }
 public:
     TreeNode* lowestCommonAncestor(TreeNode* root, TreeNode* p, TreeNode* q) {
         auto res = ancestorIfFound(root, p, q);
-        // cout<<count<<endl;
         return count == 2 ? res : NULL;
     }
 };
