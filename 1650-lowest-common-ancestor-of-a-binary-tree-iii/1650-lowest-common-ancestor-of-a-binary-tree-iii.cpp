ef05977cf4b3bf6ccb70f77477be34a357519cec
Muhtasim Ulfat tanmoy <mtanmoy5086@gmail.com>
2022-07-16 22:49:54 +0600

    Time: 22 ms (44.97%), Space: 10.3 MB (62.48%) - LeetHub

--- 1650-lowest-common-ancestor-of-a-binary-tree-iii/1650-lowest-common-ancestor-of-a-binary-tree-iii.cpp
+++ 1650-lowest-common-ancestor-of-a-binary-tree-iii/1650-lowest-common-ancestor-of-a-binary-tree-iii.cpp
@@ -9,20 +9,35 @@ public:
 };
 */
 
+// class Solution {
+// public:
+//     Node* lowestCommonAncestor(Node* p, Node * q) {
+//         Node* curr = p;
+//         unordered_set<int> bag;
+//         while(curr) {
+//             bag.insert(curr->val);
+//             curr = curr->parent;
+//         }
+//         curr = q;
+//         while(curr) {
+//             if (bag.count(curr->val)) return curr;
+//             curr = curr->parent;
+//         }
+//         return NULL;
+//     }
+// };
+
 class Solution {
 public:
     Node* lowestCommonAncestor(Node* p, Node * q) {
-        Node* curr = p;
-        unordered_set<int> bag;
-        while(curr) {
-            bag.insert(curr->val);
-            curr = curr->parent;
+        Node *nodep = p, *nodeq = q;
+        while (nodep != nodeq) {
+            nodep = nodep->parent;
+            nodeq = nodeq->parent;
+            if (nodep == nodeq) return nodep;
+            if (!nodep) nodep = q;
+            if (!nodeq) nodeq = p;
         }
-        curr = q;
-        while(curr) {
-            if (bag.count(curr->val)) return curr;
-            curr = curr->parent;
-        }
-        return NULL;
+        return nodep;
     }
 };
