d9ecee3804c0c1a99408b89e72d33078674f2b13
Muhtasim Ulfat tanmoy <mtanmoy5086@gmail.com>
2022-07-18 01:08:37 +0600

    Time: 12 ms (78.40%), Space: 12.1 MB (20.14%) - LeetHub

---
+++ 150-evaluate-reverse-polish-notation/150-evaluate-reverse-polish-notation.cpp
@@ -0,0 +1,47 @@
+class Solution {
+public:
+    int evalRPN(vector<string>& tokens) {
+        
+        function<int(string)> toInt = [](string s) -> int {
+            int res = 0;
+            for(auto c: s) { 
+                if (c == '-') continue;
+                res = res * 10; res += (c - '0'); 
+            }
+            return s[0] == '-' ? -res: res;
+        };
+        
+        stack<int> args;
+        unordered_set<char> operations = {'+', '-', '*', '/'};
+        
+        for(auto token: tokens) {
+            
+            if (token.size() == 1 && operations.count(token[0])) {
+                
+                auto secondArg = args.top(); args.pop();
+                auto firstArg = args.top(); args.pop();
+                                
+                int res = 0;
+                
+                switch (token[0]) {
+                    case '+':
+                        res = firstArg + secondArg;
+                        break;
+                    case '-':
+                        res = firstArg - secondArg;
+                        break;
+                    case '*':
+                        res = firstArg * secondArg;
+                        break;
+                    case '/':
+                        res = firstArg / secondArg;
+                }
+                
+                args.push(res);
+                
+            } else args.push(toInt(token));
+        }
+        
+        return args.top();
+    }
+};
