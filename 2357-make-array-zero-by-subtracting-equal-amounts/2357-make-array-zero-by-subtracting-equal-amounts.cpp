0a0ee8b2c0b3401553b61fab684d01de9da8b800
MuhtasimTanmoy <taaanmo0oy@gmail.com>
2022-08-02 05:29:29 +0600

    segment tree add

--- 2357-make-array-zero-by-subtracting-equal-amounts/2357-make-array-zero-by-subtracting-equal-amounts.cpp
+++
@@ -1,8 +0,0 @@
-class Solution {
-public:
-    int minimumOperations(vector<int>& nums) {
-        unordered_set<int> bag(nums.begin(), nums.end());
-        bag.erase(0);
-        return bag.size();
-    }
-};
--- 2358-maximum-number-of-groups-entering-a-competition/2358-maximum-number-of-groups-entering-a-competition.cpp
+++
@@ -1,8 +0,0 @@
-class Solution {
-public:
-    int maximumGroups(vector<int>& grades) {
-        int k = 0, total = 0, n = grades.size();
-        while (total + k < n) total += ++k;
-        return k;
-    }
-};
