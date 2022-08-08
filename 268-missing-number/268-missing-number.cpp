6aada9d687cc6133ba8b6464b12dd46fa972910d
Muhtasim Ulfat tanmoy <mtanmoy5086@gmail.com>
2022-08-09 01:49:49 +0600

    Time: 26 ms (71.54%), Space: 18 MB (19.70%) - LeetHub

--- 268-missing-number/268-missing-number.cpp
+++ 268-missing-number/268-missing-number.cpp
@@ -1,9 +1,21 @@
 class Solution {
 public:
     int missingNumber(vector<int>& nums) {
-        int sum = nums.size();
-        for (int i = 0; i < nums.size(); i++)
-            sum ^= (i ^ nums[i]);
+        int sum = 0;
+        for (int i = 0; i < nums.size(); i++) {
+            sum -=  nums[i];
+            sum += i + 1;
+        }
         return sum;
     }
 };
+
+// class Solution {
+// public:
+//     int missingNumber(vector<int>& nums) {
+//         int sum = nums.size();
+//         for (int i = 0; i < nums.size(); i++)
+//             sum ^= (i ^ nums[i]);
+//         return sum;
+//     }
+// };
