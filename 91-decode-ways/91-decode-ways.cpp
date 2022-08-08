9c6199087330340894ad31227c7c8d9d6d5974e1
Muhtasim Ulfat tanmoy <mtanmoy5086@gmail.com>
2022-08-09 03:39:41 +0600

    Time: 3 ms (61.43%), Space: 6.2 MB (74.08%) - LeetHub

--- 91-decode-ways/91-decode-ways.cpp
+++ 91-decode-ways/91-decode-ways.cpp
@@ -17,6 +17,25 @@
 // };
 
 // Iterative DP
+// class Solution {
+// public:
+//     int numDecodings(string s) {
+//         int len = s.size();
+//         auto ok = [](auto x) { return 1 <= x && x <= 26; };
+//         vector<int> dp(len + 1); dp[len] = 1;
+//         for (int i {len - 1}; i >= 0; i--) {
+//             auto one = s[i] - '0';
+//             auto two = one && i + 1 < len ? stoi(s.substr(i, 2)): 0;
+//             if (ok(one)) dp[i] += dp[i+1];
+//             if (ok(two)) dp[i] += dp[i+2];
+//         }
+//         return dp[0];
+//     }
+// };
+
+
+// constant space
+// Iterative DP
 class Solution {
 public:
     int numDecodings(string s) {
@@ -26,9 +45,11 @@ public:
         for (int i {len - 1}; i >= 0; i--) {
             auto one = s[i] - '0';
             auto two = one && i + 1 < len ? stoi(s.substr(i, 2)): 0;
+            
             int sum = 0;
             if (ok(one)) sum = first;
             if (ok(two)) sum += second;
+            
             second = first;
             first = sum;
         }
