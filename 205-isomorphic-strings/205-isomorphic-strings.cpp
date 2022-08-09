49b905941c937fb273cb47d7a5a1ab0c1d9d9fac
Muhtasim Ulfat tanmoy <mtanmoy5086@gmail.com>
2022-08-09 22:09:32 +0600

    Time: 4 ms (91.38%), Space: 7.1 MB (46.63%) - LeetHub

--- 205-isomorphic-strings/205-isomorphic-strings.cpp
+++ 205-isomorphic-strings/205-isomorphic-strings.cpp
@@ -1,13 +1,15 @@
 class Solution {
 public:
     bool isIsomorphic(string s, string t) {
-        vector<int> fw(128, -1), bw(128, -1);
-        for (int i = 0; i < s.size(); ++i)
-            if (fw[s[i]] == -1) {
-                fw[s[i]] = t[i];
-                if (bw[t[i]] != -1) return false;
-                else bw[t[i]] = s[i];
-            } else if (fw[s[i]] != t[i]) return false;
+        vector<int> scount(128, -1), tcount(128, -1);
+        for (int i = 0; i < s.size(); ++i) {
+            auto schar = s[i], tchar = t[i];
+            if (scount[schar] == -1) {
+                scount[schar] = tchar;
+                if (tcount[tchar] != -1) return false;
+                else tcount[tchar] = schar;
+            } else if (scount[schar] != tchar) return false;
+        }
         return true;
     }
 };
