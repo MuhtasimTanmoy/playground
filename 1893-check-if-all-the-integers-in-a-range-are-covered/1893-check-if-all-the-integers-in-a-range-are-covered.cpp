93d4451310588aaca037f46c253cb8b42cdde1ed
Muhtasim Ulfat tanmoy <mtanmoy5086@gmail.com>
2022-08-15 03:45:19 +0600

    Time: 3 ms (94.68%), Space: 8.9 MB (41.13%) - LeetHub

--- 1893-check-if-all-the-integers-in-a-range-are-covered/1893-check-if-all-the-integers-in-a-range-are-covered.cpp
+++ 1893-check-if-all-the-integers-in-a-range-are-covered/1893-check-if-all-the-integers-in-a-range-are-covered.cpp
@@ -1,11 +1,24 @@
+// class Solution {
+// public:
+//     bool isCovered(vector<vector<int>>& ranges, int left, int right) {
+//         vector<int> rangemark(51, 0);
+//         for (auto range: ranges)
+//             for (int i = range[0]; i <= range[1]; i++)
+//                 rangemark[i] = 1;    
+//         for (int i = left; i <= right; i++) if (!rangemark[i]) return false;
+//         return true;
+//     }
+// };
+
 class Solution {
 public:
     bool isCovered(vector<vector<int>>& ranges, int left, int right) {
-        vector<int> rangemark(51, 0);
-        for (auto range: ranges)
-            for (int i = range[0]; i <= range[1]; i++)
-                rangemark[i] = 1;    
-        for (int i = left; i <= right; i++) if (!rangemark[i]) return false;
-        return true;
+        sort(ranges.begin(), ranges.end());
+        for (auto &range: ranges) {
+            if (range[0] <= left && left <= range[1]) {
+                left = range[1]+1;
+            }
+        }
+        return left > right;
     }
 };
