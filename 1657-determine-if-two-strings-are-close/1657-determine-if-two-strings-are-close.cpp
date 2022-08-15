0e53964ed355be7640e864641f543e657d20bf45
Muhtasim Ulfat tanmoy <mtanmoy5086@gmail.com>
2022-08-15 12:43:25 +0600

    Time: 116 ms (67.04%), Space: 20.4 MB (94.11%) - LeetHub

---
+++ 1657-determine-if-two-strings-are-close/1657-determine-if-two-strings-are-close.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    bool closeStrings(string word1, string word2) {
+        if (word1.size() != word2.size()) return false;
+        vector<int> word1Map(26, 0);
+        vector<int> word2Map(26, 0);
+        for (auto c : word1)  word1Map[c - 'a']++;
+        for (auto c : word2) word2Map[c - 'a']++;
+        for (int i = 0; i < 26; i++) 
+            if ((word1Map[i] == 0 && word2Map[i] > 0) ||
+                (word2Map[i] == 0 && word1Map[i] > 0)) {
+                return false;
+            }
+        sort(word1Map.begin(), word1Map.end());
+        sort(word2Map.begin(), word2Map.end());
+        return (word1Map == word2Map);
+    }
+};
