ee08133c0048a8224ec62348245fd0ede0a55418
Muhtasim Ulfat tanmoy <mtanmoy5086@gmail.com>
2022-08-20 01:09:54 +0600

    Time: 135 ms (70.13%), Space: 58.5 MB (41.56%) - LeetHub

--- 659-split-array-into-consecutive-subsequences/659-split-array-into-consecutive-subsequences.cpp
+++ 659-split-array-into-consecutive-subsequences/659-split-array-into-consecutive-subsequences.cpp
@@ -3,18 +3,17 @@ public:
     bool isPossible(vector<int>& nums) {
         unordered_map<int, int> cnt, len;
         for (auto &num: nums) ++cnt[num];
-    
         for (auto &num: nums) {
             if (!cnt[num]) continue;
             --cnt[num];
             
-            if (len[num - 1]) {
-                --len[num - 1];
-                ++len[num];
+            if (len[num]) {
+                --len[num];
+                ++len[num + 1];
             } else if (cnt[num + 1] && cnt[num + 2]) {
                 --cnt[num + 1];
                 --cnt[num + 2];
-                ++len[num + 2];
+                ++len[num + 3];
             } else return false;
         }
         return true;
