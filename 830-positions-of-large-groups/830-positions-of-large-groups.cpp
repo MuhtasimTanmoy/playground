51954ea3eba5e1b5f4762e641a53ae3daf17d4a1
Muhtasim Ulfat tanmoy <mtanmoy5086@gmail.com>
2022-08-22 01:45:21 +0600

    Time: 2 ms (86.50%), Space: 7.4 MB (56.17%) - LeetHub

--- 830-positions-of-large-groups/830-positions-of-large-groups.cpp
+++ 830-positions-of-large-groups/830-positions-of-large-groups.cpp
@@ -4,15 +4,14 @@ public:
         vector<vector<int>> res;
         char prev = '0';
         int start = 0, end = 0;
-        for (int i = 0; i < s.size(); i++) {
-            if (s[i] != prev) {
+        for (int i = 0; i <= s.size(); i++) {
+            if (s[i] != prev || i == s.size()) {
                 prev = s[i];
                 if (end - start >= 3) res.push_back({start, end - 1});
                 start = i;
             }
             end++;
         }
-        if (end - start >= 3) res.push_back({start, end - 1});
         return res;
     }
 };
