a628c4658ae302adda175284b689960d8deef955
Muhtasim Ulfat tanmoy <mtanmoy5086@gmail.com>
2022-08-24 02:22:52 +0600

    Time: 609 ms (96.05%), Space: 238.5 MB (29.12%) - LeetHub

--- 2080-range-frequency-queries/2080-range-frequency-queries.cpp
+++ 2080-range-frequency-queries/2080-range-frequency-queries.cpp
@@ -2,8 +2,9 @@ class RangeFreqQuery {
     vector<int> m[10000];
 public:
     RangeFreqQuery(vector<int>& arr) {
-        for (int i = 0; i < arr.size(); i++) 
+        for (int i = 0; i < arr.size(); i++) {
             m[arr[i] - 1].push_back(i);
+        }
     }
     int query(int left, int right, int value) {
         const auto& arr = m[value - 1];    
