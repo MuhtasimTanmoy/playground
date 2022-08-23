7bebcd2c57b55d20e25093fc2fd27290d17d939f
Muhtasim Ulfat tanmoy <mtanmoy5086@gmail.com>
2022-08-23 21:24:02 +0600

    Time: 18 ms (77.08%), Space: 10.8 MB (85.69%) - LeetHub

--- 2023-number-of-pairs-of-strings-with-concatenation-equal-to-target/2023-number-of-pairs-of-strings-with-concatenation-equal-to-target.cpp
+++ 2023-number-of-pairs-of-strings-with-concatenation-equal-to-target/2023-number-of-pairs-of-strings-with-concatenation-equal-to-target.cpp
@@ -1,16 +1,34 @@
+// class Solution {
+// public:
+//     int numOfPairs(vector<string>& nums, string target) {
+//         int count = 0;
+//         auto targetHash = hash<string>{}(target);
+//         for (int i = 0; i < nums.size() ; i++) 
+//             for (int j = 0; j < nums.size(); j++) {
+//                 if (i == j) continue;
+//                 if (nums[i].size() + nums[j].size() != target.size()) continue;
+//                 auto concat = nums[i] + nums[j];
+//                 auto hashConcat = hash<string>{}(concat);
+//                 if (hashConcat == targetHash) count++;
+//             }
+//         return count;
+//     }
+// };
+
+
 class Solution {
 public:
     int numOfPairs(vector<string>& nums, string target) {
-        int count = 0;
-        auto targetHash = hash<string>{}(target);
-        for (int i = 0; i < nums.size() ; i++) 
-            for (int j = 0; j < nums.size(); j++) {
-                if (i == j) continue;
-                if (nums[i].size() + nums[j].size() != target.size()) continue;
-                auto concat = nums[i] + nums[j];
-                auto hashConcat = hash<string>{}(concat);
-                if (hashConcat == targetHash) count++;
-            }
+        unordered_map<string,int> m;
+        int n = nums.size();
+        for (int i = 0; i < n; i++) m[nums[i]]++;
+        int count = 0, l = target.length();
+        for (int i = 0; i < l; i++) {
+            string s=target.substr(0, i + 1);
+            string t=target.substr(i + 1, l - i - 1);
+            if (m.count(s) && m.count(t))
+                count += m[s] * ( m[t] - (s == t ? 1: 0));
+        }
         return count;
     }
 };
