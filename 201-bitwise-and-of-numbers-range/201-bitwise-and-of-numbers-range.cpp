5a0760008d795d229f3c444d585e0516b739feca
Muhtasim Ulfat tanmoy <mtanmoy5086@gmail.com>
2022-09-11 05:26:32 +0600

    Time: 30 ms (15.42%), Space: 5.9 MB (32.89%) - LeetHub

--- 201-bitwise-and-of-numbers-range/201-bitwise-and-of-numbers-range.cpp
+++ 201-bitwise-and-of-numbers-range/201-bitwise-and-of-numbers-range.cpp
@@ -12,15 +12,35 @@
 //     }
 // };
 
+// class Solution {
+// public:
+//     int rangeBitwiseAnd(int left, int right) {
+//         int m = left, n = right, shift = 0;
+//         while (m != n) {
+//             m >>= 1;
+//             n >>= 1;
+//             shift++;
+//         }
+//         return m << shift;
+//     }
+// };
+
+
+// The secret sauce of the Brian Kernighan's algorithm can be summarized as follows:
+
+// When we do AND bit operation between number and number-1, the rightmost bit of one in the original number would be turned off (from one to zero).
+
+// The idea is that for a given range [m, n][m,n] (i.e. m < nm<n), we could iteratively apply the trick on the number nn to turn off its rightmost bit of one until it becomes less or equal than the beginning of the range (mm), which we denote as n'n 
+// ′
+//  . Finally, we do AND operation between n'n 
+// ′
+//   and mm to obtain the final result.
+
 class Solution {
 public:
     int rangeBitwiseAnd(int left, int right) {
         int m = left, n = right, shift = 0;
-        while (m != n) {
-            m >>= 1;
-            n >>= 1;
-            shift++;
-        }
-        return m << shift;
+        while (m < n)  n = n & (n - 1);
+        return m & n;
     }
 };
