cc25180a7c8bb4fd069f197a2a50240efbaad439
Muhtasim Ulfat tanmoy <mtanmoy5086@gmail.com>
2022-09-10 17:23:45 +0600

    Time: 19 ms (55.34%), Space: 3 MB (65.85%) - LeetHub

--- 121-best-time-to-buy-and-sell-stock/121-best-time-to-buy-and-sell-stock.rs
+++ 121-best-time-to-buy-and-sell-stock/121-best-time-to-buy-and-sell-stock.rs
@@ -1,7 +1,7 @@
 impl Solution {
     pub fn max_profit(prices: Vec<i32>) -> i32 {
         let mut res = 0;
-        let mut minSoFar = std::i32::MAX;
+        let mut minSoFar = prices[0];
         for price in prices {
             res = res.max(price - minSoFar);
             minSoFar = minSoFar.min(price);
