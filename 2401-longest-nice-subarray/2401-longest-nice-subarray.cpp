7b5d2b1f28e6d36924daf4d5fe6df6c8568c4c54
Muhtasim Ulfat tanmoy <mtanmoy5086@gmail.com>
2022-09-11 01:53:56 +0600

    Time: 435 ms (33.14%), Space: 57.2 MB (17.75%) - LeetHub

--- 2401-longest-nice-subarray/2401-longest-nice-subarray.cpp
+++ 2401-longest-nice-subarray/2401-longest-nice-subarray.cpp
@@ -8,17 +8,31 @@ bitwise AND of every pair of elements that are in different positions in the sub
 
 */
 
+// class Solution {
+// public:
+//     int longestNiceSubarray(vector<int>& nums) {
+//         int res = 1;
+//         for (int i = 0; i < nums.size(); i++) {
+//             int orResult = nums[i];
+//             for (int j = i + 1; j < nums.size(); j++)
+//                 if ( (orResult & nums[j]) == 0 ) {
+//                     orResult |= nums[j];
+//                     res = max(res, j - i + 1);
+//                 } else break;
+//         }
+//         return res;
+//     }
+// };
+
+
 class Solution {
 public:
     int longestNiceSubarray(vector<int>& nums) {
-        int res = 1;
-        for (int i = 0; i < nums.size(); i++) {
-            int orResult = nums[i];
-            for (int j = i + 1; j < nums.size(); j++)
-                if ( (orResult & nums[j]) == 0 ) {
-                    orResult |= nums[j];
-                    res = max(res, j - i + 1);
-                } else break;
+        int res = 0, used = 0;
+        for (int j = 0, i = 0; j < nums.size(); j++) {
+            while (used & nums[j]) used ^= nums[i++];
+            used |= nums[j];
+            res = max(res, j - i + 1);
         }
         return res;
     }
