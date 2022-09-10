3f3a33467d66509611d063f4fef2b5c12db26422
Muhtasim Ulfat tanmoy <mtanmoy5086@gmail.com>
2022-09-11 05:31:37 +0600

    Time: 0 ms (100.00%), Space: 5.8 MB (76.12%) - LeetHub

---
+++ 461-hamming-distance/461-hamming-distance.cpp
@@ -0,0 +1,12 @@
+class Solution {
+public:
+    int hammingDistance(int x, int y) {
+        int i = 31, res = 0;
+        while (i--) {
+            auto xPos = (x >> i) & 1;
+            auto yPos = (y >> i) & 1;
+            if ( xPos != yPos ) res++;
+        }
+        return res;
+    }
+};
