a2b57b0b67d392d9a04310da5486dae4e7619d4b
Muhtasim Ulfat tanmoy <mtanmoy5086@gmail.com>
2022-09-14 22:24:01 +0600

    Time: 633 ms (46.65%), Space: 178.7 MB (97.90%) - LeetHub

--- 1457-pseudo-palindromic-paths-in-a-binary-tree/1457-pseudo-palindromic-paths-in-a-binary-tree.cpp
+++ 1457-pseudo-palindromic-paths-in-a-binary-tree/1457-pseudo-palindromic-paths-in-a-binary-tree.cpp
@@ -9,11 +9,12 @@
  *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
  * };
  */
+
+/*
 class Solution {
     bool isPalindrome(vector<int>& li) {
         int oddCount = 0, len = 0;
-        for (auto count: li)
-            if (count & 1) oddCount++;
+        for (auto count: li) if (count & 1) oddCount++;
         return oddCount <= 1;
     }
     
@@ -36,5 +37,27 @@ public:
         if (!root) return 0;
         vector<int> li(10, 0);
         return countPseudoPalindromicPaths(root, li);
+    }
+};
+
+*/
+
+class Solution {
+    int countPseudoPalindromicPaths(TreeNode* root, int path) {
+        if (!root) return 0;
+        path ^= 1 << root->val;
+        if (!root->left && !root->right) {
+            auto res = ((path & (path - 1)) == 0) ? 1: 0;
+            return res;
+        }
+        int left = countPseudoPalindromicPaths(root->left, path);
+        int right = countPseudoPalindromicPaths(root->right, path);
+        return left + right;
+    }
+    
+public:
+    int pseudoPalindromicPaths (TreeNode* root) {
+        if (!root) return 0;
+        return countPseudoPalindromicPaths(root, 0);
     }
 };
