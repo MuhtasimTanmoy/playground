6756ac2055ddd56b6c819f92a9d6cb48da81bdc1
Muhtasim Ulfat tanmoy <mtanmoy5086@gmail.com>
2022-09-17 05:05:24 +0600

    Time: 325 ms (99.68%), Space: 93.4 MB (59.68%) - LeetHub

--- 2381-shifting-letters-ii/2381-shifting-letters-ii.cpp
+++ 2381-shifting-letters-ii/2381-shifting-letters-ii.cpp
@@ -2,7 +2,7 @@ class Solution {
 public:
     string shiftingLetters(string s, vector<vector<int>>& shifts) {
         int len = s.length();
-        vector<int> dir(len+1, 0);
+        vector<int> dir(len + 1, 0);
         for (auto &shift: shifts) {
             int start = shift[0], end = shift[1];
             if (shift[2] == 1) dir[start]++, dir[end+1]--;
@@ -11,9 +11,9 @@ public:
         long long job = 0;
         for (int i = 0; i < len; i++) {
             job += dir[i];
-            s[i] = ((s[i]-'a') + job + 260000000LL) % 26 + 'a';
+            auto increaseBy = (s[i] - 'a' + job) % 26;
+            s[i] = (increaseBy + 26) % 26 + 'a';
         }
         return s;
     }
 };
-
