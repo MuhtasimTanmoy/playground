f48eedd00bd5b1b1081ac36611d2d14f922146e6
Muhtasim Ulfat tanmoy <mtanmoy5086@gmail.com>
2022-09-18 05:24:56 +0600

    Time: 12 ms (20.20%), Space: 7.8 MB (42.57%) - LeetHub

--- 2395-find-subarrays-with-equal-sum/2395-find-subarrays-with-equal-sum.cpp
+++ 2395-find-subarrays-with-equal-sum/2395-find-subarrays-with-equal-sum.cpp
@@ -10,3 +10,11 @@ public:
         return false;
     }
 };
+
+// bool findSubarrays(vector<int>& nums) {
+//     unordered_set<int> s;
+//     for (int i = 1; i < nums.size(); ++i)
+//         if(!s.insert(nums[i - 1] + nums[i]).second)
+//             return true;
+//     return false;
+// }
