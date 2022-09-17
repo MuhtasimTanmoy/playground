f896dd8b1f478be08d2ac6049577211ca66b949a
Muhtasim Ulfat tanmoy <mtanmoy5086@gmail.com>
2022-09-18 04:56:12 +0600

    Time: 547 ms (17.82%), Space: 102.1 MB (94.92%) - LeetHub

--- 2391-minimum-amount-of-time-to-collect-garbage/2391-minimum-amount-of-time-to-collect-garbage.cpp
+++ 2391-minimum-amount-of-time-to-collect-garbage/2391-minimum-amount-of-time-to-collect-garbage.cpp
@@ -15,3 +15,13 @@ public:
         return cost;
     }
 };
+
+// int garbageCollection(vector<string>& garb, vector<int>& travel) {
+//     int dist[128] = {};
+//     partial_sum(begin(travel), end(travel), begin(travel));
+//     for (int i = 1; i < garb.size(); ++i)
+//         for (auto g : garb[i])
+//             dist[g] = travel[i - 1];
+//     return accumulate(begin(garb), end(garb), 0, [](int sz, const auto& g) { return sz + g.size(); })
+//         + accumulate(begin(dist), end(dist), 0);
+// }
