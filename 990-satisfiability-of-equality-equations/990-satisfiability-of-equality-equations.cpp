aab0fc39d2eabe9e7c4a19ecabf83c1ba4e6e041
Muhtasim Ulfat tanmoy <mtanmoy5086@gmail.com>
2022-09-26 07:49:22 +0600

    Time: 6 ms (82.73%), Space: 11.4 MB (70.01%) - LeetHub

--- 990-satisfiability-of-equality-equations/990-satisfiability-of-equality-equations.cpp
+++ 990-satisfiability-of-equality-equations/990-satisfiability-of-equality-equations.cpp
@@ -21,6 +21,7 @@
 //     }
 // };
 
+/*
 class Solution {
 public:
     bool equationsPossible(vector<string>& equations) {
@@ -54,4 +55,38 @@ public:
         return true;
     }
 };
+*/
 
+class Solution {
+public:
+    bool equationsPossible(vector<string>& equations) {
+        constexpr int SIZE = 26;
+        vector<int> root(SIZE);
+        iota(root.begin(), root.end(), 0);
+
+        function<int(int)> find = [&](int x) {
+            if (root[x] != x) root[x] = find(root[x]);
+            return root[x];
+        };
+
+        auto unionSet = [&](int x, int y) {
+            int rx = find(x), ry = find(y);
+            root[rx] = ry;
+        };
+
+        for (string& eqn : equations)
+            if (eqn[1] == '=') {
+                int x = eqn[0] - 'a';
+                int y = eqn[3] - 'a';
+                unionSet(x, y);
+            }
+
+        for (string& eqn : equations)
+            if (eqn[1] == '!') {
+                int x = eqn[0] - 'a';
+                int y = eqn[3] - 'a';
+                if (find(x) == find(y)) return false;
+            }
+        return true;
+    }
+};
