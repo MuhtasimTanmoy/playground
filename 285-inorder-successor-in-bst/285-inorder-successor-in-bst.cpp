ec92c352ea0af7ee3cc2c3c70bf46a44d5464cea
Muhtasim Ulfat tanmoy <mtanmoy5086@gmail.com>
2022-10-05 23:44:02 +0600

    Time: 58 ms (38.13%), Space: 22.9 MB (9.96%) - LeetHub

--- 285-inorder-successor-in-bst/285-inorder-successor-in-bst.cpp
+++ 285-inorder-successor-in-bst/285-inorder-successor-in-bst.cpp
@@ -7,42 +7,46 @@
  *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
  * };
  */
+// class Solution {
+//     TreeNode* cache = NULL;
+//     TreeNode* res = NULL;
+// public:
+//     TreeNode* inorderSuccessor(TreeNode* root, TreeNode* p) {
+//         if (!root) return NULL;
+//         if (p->right) {
+//             auto item = p->right;
+//             while (item->left) item = item->left;
+//             return item;
+//         }
+//         traverse(root, p);
+//         return res;
+//     }
+    
+//     void traverse(TreeNode* root, TreeNode* p) {
+//         if (!root) return;
+//         traverse(root->left, p);
+//         if (cache == p && !res) { 
+//             cout<<root->val<<endl;
+//             res = root; return; }
+//         cache = root;
+//         traverse(root->right, p);
+//     }
+// };
+
+
 class Solution {
     TreeNode* cache = NULL;
-    TreeNode* res = NULL;
 public:
     TreeNode* inorderSuccessor(TreeNode* root, TreeNode* p) {
-        if (!root) return NULL;
         if (p->right) {
-            auto item = p->right;
-            while (item->left) item = item->left;
-            return item;
+            p = p->right;
+            while (p->left) p = p->left;
+            return p;
         }
-        traverse(root, p);
-        return res;
-    }
-    
-    void traverse(TreeNode* root, TreeNode* p) {
-        if (!root) return;
-        traverse(root->left, p);
-        if (cache == p && !res) { 
-            cout<<root->val<<endl;
-            res = root; return; }
-        cache = root;
-        traverse(root->right, p);
+        
+        TreeNode* candidate = NULL;
+        while (root != p)
+            root = p->val > root->val ? root->right : (candidate = root)->left;
+        return candidate;
     }
 };
-
-// TreeNode* inorderSuccessor(TreeNode* root, TreeNode* p) {
-//     if (p->right) {
-//         p = p->right;
-//         while (p->left)
-//             p = p->left;
-//         return p;
-//     }
-//     TreeNode* candidate = NULL;
-//     while (root != p)
-//         root = (p->val > root->val) ? 
-//         root->right : (candidate = root)->left;
-//     return candidate;
-// }
