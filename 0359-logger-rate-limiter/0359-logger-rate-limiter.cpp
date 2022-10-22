eeafce45b847a019c103979c08ab7c30a5b07fd4
Muhtasim Ulfat tanmoy <mtanmoy5086@gmail.com>
2022-10-23 00:33:40 +0600

    Time: 71 ms (98.30%), Space: 32.6 MB (27.55%) - LeetHub

--- 0359-logger-rate-limiter/0359-logger-rate-limiter.cpp
+++ 0359-logger-rate-limiter/0359-logger-rate-limiter.cpp
@@ -2,17 +2,19 @@ class Logger {
     unordered_map<string, int> bag;
 public:
     Logger() { }
-    
     bool shouldPrintMessage(int timestamp, string message) {
-        if (bag.count(message)) {
-            auto prevTime = bag[message];
-            if (timestamp - prevTime >= 10) {
-                bag[message] = timestamp;
-                return true;
-            } else return false;
-        } else {
+        
+        if (!bag.count(message)) {
             bag[message] = timestamp;
             return true;
         }
+        
+        auto prevTime = bag[message];
+        if (timestamp - prevTime >= 10) {
+            bag[message] = timestamp;
+            return true;
+        } 
+        
+        return false;
     }
 };
