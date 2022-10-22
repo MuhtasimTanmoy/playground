a3c6d9039c2f8a3c970415f7445c09dd61c5cc7b
Muhtasim Ulfat tanmoy <mtanmoy5086@gmail.com>
2022-10-22 19:22:53 +0600

    Time: 240 ms (41.72%), Space: 45.5 MB (67.16%) - LeetHub

--- 0215-kth-largest-element-in-an-array/0215-kth-largest-element-in-an-array.cpp
+++ 0215-kth-largest-element-in-an-array/0215-kth-largest-element-in-an-array.cpp
@@ -32,27 +32,61 @@
 //     }
 // };
 
+// class Solution {
+// public:
+//     int findKthLargest(vector<int>& nums, int k) {
+//         //partition rule: >=pivot   pivot   <=pivot
+//         int left = 0, right = nums.size() - 1, idx = 0;
+//         while(1) {
+//             idx = partition(nums, left, right);
+//             if(idx == k-1) break;
+//             else if (idx < k-1) left = idx + 1;
+//             else right= idx - 1;
+//         }
+//         return nums[idx];
+//     }
+    
+//     int partition(vector<int>& nums,int left,int right) {
+//         int pivot = nums[left], l = left + 1, r = right;
+//         while (l <= r) {
+//             if (nums[l] < pivot && nums[r] > pivot) swap(nums[l++], nums[r--]);
+//             if (nums[l] >= pivot) ++l;
+//             if (nums[r] <= pivot) --r;
+//         }
+//         swap(nums[left], nums[r]);
+//         return r;
+//     }
+// };
+
+
 class Solution {
+private:
+    int partition(vector<int>& nums, int l, int r) {
+        if (l == r) return l;
+        int idx=l;
+        int randpivot=rand()%(r-l)+l; // make the pivot random
+        swap(nums[randpivot],nums[r]);
+        
+        for (int i=l; i<r; i++) {
+            if (nums[i]>nums[r]) {
+                swap(nums[idx],nums[i]);
+                idx++;
+            }
+        }
+        swap(nums[idx],nums[r]);
+        return idx;
+    }
 public:
     int findKthLargest(vector<int>& nums, int k) {
-        //partition rule: >=pivot   pivot   <=pivot
-        int left=0,right=nums.size()-1,idx=0;
-        while(1){
-            idx = partition(nums,left,right);
-            if(idx==k-1) break;
-            else if(idx < k-1) left=idx+1;
-            else right= idx-1;
+        int l=0,n=nums.size(),r=n-1;
+        while (l<=r) {
+            int pivot=partition(nums,l,r);
+            if (pivot<k-1) {
+                l=pivot+1;
+            } else if (pivot>k-1) {
+                r=pivot-1;
+            } else return nums[pivot];
         }
-        return nums[idx];
-    }
-    int partition(vector<int>& nums,int left,int right) {
-        int pivot = nums[left], l=left+1, r = right;
-        while(l<=r){
-            if(nums[l]<pivot && nums[r]>pivot) swap(nums[l++],nums[r--]);
-            if(nums[l]>=pivot) ++l;
-            if(nums[r]<=pivot) --r;
-        }
-        swap(nums[left], nums[r]);
-        return r;
+        return INT_MIN;
     }
 };
