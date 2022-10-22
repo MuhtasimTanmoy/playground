0ac30b096ce9754c891af8da6548bd674e172dd8
Muhtasim Ulfat tanmoy <mtanmoy5086@gmail.com>
2022-10-22 20:08:25 +0600

    Time: 100 ms (97.58%), Space: 45.5 MB (86.92%) - LeetHub

--- 0215-kth-largest-element-in-an-array/0215-kth-largest-element-in-an-array.cpp
+++ 0215-kth-largest-element-in-an-array/0215-kth-largest-element-in-an-array.cpp
@@ -32,59 +32,60 @@
 //     }
 // };
 
-// class Solution {
-// public:
-//     int findKthLargest(vector<int>& nums, int k) {
-//         //partition rule: >=pivot   pivot   <=pivot
-//         int left = 0, right = nums.size() - 1, idx = 0;
-//         while(1) {
-//             idx = partition(nums, left, right);
-//             if(idx == k-1) break;
-//             else if (idx < k-1) left = idx + 1;
-//             else right= idx - 1;
-//         }
-//         return nums[idx];
-//     }
-    
-//     int partition(vector<int>& nums,int left,int right) {
-//         int pivot = nums[left], l = left + 1, r = right;
-//         while (l <= r) {
-//             if (nums[l] < pivot && nums[r] > pivot) swap(nums[l++], nums[r--]);
-//             if (nums[l] >= pivot) ++l;
-//             if (nums[r] <= pivot) --r;
-//         }
-//         swap(nums[left], nums[r]);
-//         return r;
-//     }
-// };
-
-
 class Solution {
-private:
-    int partition(vector<int>& nums, int l, int r) {
-        if (l == r) return l;
-        
-        int idx = l;
-        int randpivot = rand() % (r - l) + l; // make the pivot random
-        swap(nums[randpivot], nums[r]);
-        
-        for (int i = l; i < r; i++)
-            if (nums[i] > nums[r]) {
-                swap(nums[idx], nums[i]);
-                idx++;
-            }
-        swap(nums[idx], nums[r]);
-        return idx;
-    }
 public:
     int findKthLargest(vector<int>& nums, int k) {
-        int l = 0, r = nums.size() - 1;
-        while (l <= r) {
-            int pivot = partition(nums, l, r);
-            if (pivot < k - 1) l = pivot + 1;
-            else if (pivot > k - 1) r = pivot - 1;
-            else return nums[pivot];
+        
+        auto pt = [&](int left, int right) {
+            if (left == right) return left;
+            int rnd = random() % (right - left) + left;
+            swap(nums[rnd], nums[right]);
+            int index = left;
+            for (int i = left; i < right; i++)
+                if (nums[i] > nums[right]) {
+                    swap(nums[index], nums[i]);
+                    index++;
+                }
+            swap(nums[index], nums[right]);
+            return index;
+        };
+        
+        auto left = 0, right = (int)nums.size() - 1;
+        while (left <= right) {
+            auto partitionPoint = pt(left, right);
+            if (partitionPoint > k - 1) right = partitionPoint - 1;
+            else if (partitionPoint < k - 1) left = partitionPoint + 1;
+            else return nums[partitionPoint];
         }
-        return INT_MIN;
+        throw "INVALID Partition key";
     }
 };
+
+
+// class Solution {
+// private:
+//     int partition(vector<int>& nums, int l, int r) {
+//         if (l == r) return l;
+//         int idx = l, randpivot = rand() % (r - l) + l; // make the pivot random
+//         swap(nums[randpivot], nums[r]);
+//         for (int i = l; i < r; i++)
+//             if (nums[i] > nums[r]) {
+//                 swap(nums[idx], nums[i]);
+//                 idx++;
+//             }
+//         swap(nums[idx], nums[r]);
+//         return idx;
+//     }
+    
+// public:
+//     int findKthLargest(vector<int>& nums, int k) {
+//         int l = 0, r = nums.size() - 1;
+//         while (l <= r) {
+//             int pivot = partition(nums, l, r);
+//             if (pivot < k - 1) l = pivot + 1;
+//             else if (pivot > k - 1) r = pivot - 1;
+//             else return nums[pivot];
+//         }
+//         return INT_MIN;
+//     }
+// };
