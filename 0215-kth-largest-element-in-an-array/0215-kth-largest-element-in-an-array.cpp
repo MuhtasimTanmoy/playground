fac7fc67a4352fec226e01bafcfa166d7daf7e77
Muhtasim Ulfat tanmoy <mtanmoy5086@gmail.com>
2022-10-22 19:24:42 +0600

    Time: 251 ms (35.95%), Space: 45.6 MB (67.16%) - LeetHub

--- 0215-kth-largest-element-in-an-array/0215-kth-largest-element-in-an-array.cpp
+++ 0215-kth-largest-element-in-an-array/0215-kth-largest-element-in-an-array.cpp
@@ -59,34 +59,70 @@
 // };
 
 
-class Solution {
-private:
-    int partition(vector<int>& nums, int l, int r) {
-        if (l == r) return l;
-        int idx = l;
-        int randpivot = rand() % (r-l) +l; // make the pivot random
-        swap(nums[randpivot], nums[r]);
+// class Solution {
+// private:
+//     int partition(vector<int>& nums, int l, int r) {
+//         if (l == r) return l;
+//         int idx = l;
+//         int randpivot = rand() % (r-l) +l; // make the pivot random
+//         swap(nums[randpivot], nums[r]);
         
-        for (int i = l; i < r; i++) {
-            if (nums[i] > nums[r]) {
-                swap(nums[idx],nums[i]);
-                idx++;
+//         for (int i = l; i < r; i++) {
+//             if (nums[i] > nums[r]) {
+//                 swap(nums[idx],nums[i]);
+//                 idx++;
+//             }
+//         }
+//         swap(nums[idx],nums[r]);
+//         return idx;
+//     }
+// public:
+//     int findKthLargest(vector<int>& nums, int k) {
+//         int l=0,n=nums.size(),r=n-1;
+//         while (l<=r) {
+//             int pivot=partition(nums,l,r);
+//             if (pivot<k-1) {
+//                 l=pivot+1;
+//             } else if (pivot>k-1) {
+//                 r=pivot-1;
+//             } else return nums[pivot];
+//         }
+//         return INT_MIN;
+//     }
+// };
+
+class Solution {
+    int partition(vector<int> &nums, int low, int high){
+        int pivot_pos = rand() % (high-low+1)+low;  // random select pivot position(ensure the selected index between low and high index)
+        int pivot = nums[pivot_pos];
+        swap(nums[pivot_pos],nums[high]);
+		//----below is classical quick sort process----
+        int l = low-1;
+        for(int i = low; i<high; i++){
+            if(nums[i]<pivot){
+                l++;
+                swap(nums[l],nums[i]);
             }
         }
-        swap(nums[idx],nums[r]);
-        return idx;
+        swap(nums[l+1],nums[high]);
+        return l+1;
     }
 public:
     int findKthLargest(vector<int>& nums, int k) {
-        int l=0,n=nums.size(),r=n-1;
-        while (l<=r) {
-            int pivot=partition(nums,l,r);
-            if (pivot<k-1) {
-                l=pivot+1;
-            } else if (pivot>k-1) {
-                r=pivot-1;
-            } else return nums[pivot];
+        int low = 0;
+        int high = nums.size()-1;
+        int rank = nums.size()-k;  //turn problem to find rank-th smallest
+        while(true){
+            int pivot_pos = partition(nums,low,high);
+            if(pivot_pos == rank){
+                return nums[pivot_pos];
+            }
+            if(pivot_pos<rank){   // if pivot_pos smaller than the rank, update low bound
+                low = pivot_pos+1;
+            }
+            else{
+                high = pivot_pos-1;
+            }
         }
-        return INT_MIN;
     }
 };
