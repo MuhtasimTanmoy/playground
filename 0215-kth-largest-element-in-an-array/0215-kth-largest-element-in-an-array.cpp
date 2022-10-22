d3ef52da7c3edf97e8f9d5d0804f008faf63f115
Muhtasim Ulfat tanmoy <mtanmoy5086@gmail.com>
2022-10-22 18:08:03 +0600

    Time: 259 ms (31.54%), Space: 47.3 MB (32.21%) - LeetHub

--- 0215-kth-largest-element-in-an-array/0215-kth-largest-element-in-an-array.cpp
+++ 0215-kth-largest-element-in-an-array/0215-kth-largest-element-in-an-array.cpp
@@ -1,11 +1,4 @@
-class Solution {
-public:
-    int findKthLargest(vector<int>& nums, int k) {
-        sort(nums.begin(), nums.end());
-        return nums[nums.size() - k];
-    }
-};
-
+// O(n log n) running time + O(1) memory
 // class Solution {
 // public:
 //     int findKthLargest(vector<int>& nums, int k) {
@@ -13,3 +6,28 @@ public:
 //         return nums[nums.size() - k];
 //     }
 // };
+
+
+// O(N log K) running time + O(K) memory
+// class Solution {
+// public:
+//     int findKthLargest(vector<int>& nums, int k) {
+//         priority_queue<int> pq;
+//         for (auto num: nums)  pq.push(num);
+//         while (--k) pq.pop();
+//         return pq.top();
+//     }
+// };
+
+
+class Solution {
+public:
+    int findKthLargest(vector<int>& nums, int k) {
+        priority_queue<int, vector<int>, greater<int>> pq;
+        for (auto num: nums) {
+            pq.push(num);
+            if (pq.size() > k) pq.pop();
+        }
+        return pq.top();            
+    }
+};
