7ba6192c49034f759e0fbbd9cd2c5f931298f048
Muhtasim Ulfat tanmoy <mtanmoy5086@gmail.com>
2022-10-24 07:20:57 +0600

    Time: 489 ms (76.45%), Space: 174 MB (41.15%) - LeetHub

--- 0146-lru-cache/0146-lru-cache.cpp
+++ 0146-lru-cache/0146-lru-cache.cpp
@@ -1,39 +1,79 @@
 class LRUCache {
-list<pair<int, int>> dll;
-unordered_map<int, list<pair<int, int>>::iterator> m;
-int capacity;
 public:
-    LRUCache(int _capacity) {
-        capacity = _capacity;
-    }
-
+    LRUCache(int capacity): capacity(capacity) {}
+    
     int get(int key) {
-        if(m.count(key) == 0) return -1;
-        auto itr = m[key];
+        if (!storage.count(key)) return -1;
+        
+        auto itr = storage[key];
         auto item = *itr;
         dll.erase(itr);
+        
         dll.push_front(item);
-        m[key] = dll.begin();
+        storage[key] = dll.begin();
         return item.second;
     }
-
+    
     void put(int key, int value) {
-        if(m.count(key) == 1) {
-            // If key exists, update value for that key
-            auto itr = m[key];
+        
+        if (storage.count(key)) {
+            auto itr = storage[key];
             dll.erase(itr);
-            dll.push_front({key, value});
-            m[key] = dll.begin();
-        } else {
-            if(dll.size() >= capacity) {
-                // Evict LRU
-                auto item = dll.back();
-                dll.pop_back();
-                m.erase(item.first);
-            }
-            // Insert new key, val
-            dll.push_front({key, value});
-            m[key] = dll.begin();
+            dll.emplace_front(key, value);
+            storage[key] = dll.begin();
+            return;
         }
+        
+        if (dll.size() == capacity) {
+            auto item = dll.back();
+            dll.pop_back();
+            storage.erase(item.first);
+        }
+        
+        dll.emplace_front(key, value);
+        storage[key] = dll.begin();
     }
+private:
+    list<pair<int, int>> dll;
+    unordered_map<int, list<pair<int, int>>::iterator> storage;
+    int capacity;
 };
+
+
+// class LRUCache {
+    
+//     int cap;
+//     list< pair<int, int> > holder;
+//     unordered_map<int, list< pair<int, int> >::iterator> indexMapper;
+        
+// public:
+//     LRUCache(int capacity) {
+//         cap = capacity;
+//     }
+    
+//     int get(int key) {
+//         if (!indexMapper.count(key)) return -1;
+        
+//         auto itr = *indexMapper[key];
+//         holder.erase(indexMapper[key]);
+//         holder.insert(holder.begin(), itr);
+        
+//         indexMapper[key] = holder.begin();
+//         return itr.second;
+//     }
+    
+//     void put(int key, int value) {
+//         if (get(key) != -1) 
+//             indexMapper[key]->second = value;
+//         else {
+//             pair<int, int> item = {key, value};
+//             holder.insert(holder.begin(), item);
+//             indexMapper[key] = holder.begin();
+            
+//             if (holder.size() > cap) {
+//                 indexMapper.erase(holder.back().first);
+//                 holder.pop_back();
+//             }
+//         }
+//     }
+// };
