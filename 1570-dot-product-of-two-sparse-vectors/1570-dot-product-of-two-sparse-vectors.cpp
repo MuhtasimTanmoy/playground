ea9120b15c79526cf872682abeee59746f59e667
Muhtasim Ulfat tanmoy <mtanmoy5086@gmail.com>
2022-10-29 05:13:54 +0600

    Time: 223 ms (96.38%), Space: 172.8 MB (27.73%) - LeetHub

--- 1570-dot-product-of-two-sparse-vectors/1570-dot-product-of-two-sparse-vectors.cpp
+++ 1570-dot-product-of-two-sparse-vectors/1570-dot-product-of-two-sparse-vectors.cpp
@@ -7,8 +7,10 @@ public:
     
     int dotProduct(SparseVector& vec) {
         int res = 0;
-        for (int i = 0; i < vec.list.size(); i++) 
+        for (int i = 0; i < vec.list.size(); i++) {
+            if (!list[i] || !vec.list[i]) continue;
             res += list[i] * vec.list[i];
+        }
         return res;
     }    
 };
