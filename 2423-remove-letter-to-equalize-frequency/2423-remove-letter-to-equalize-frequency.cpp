98d08d87d074f400cff801418e95741846f236a9
Muhtasim Ulfat tanmoy <mtanmoy5086@gmail.com>
2022-10-31 14:25:21 +0600

    Time: 11 ms (10.40%), Space: 7.5 MB (5.74%) - LeetHub

---
+++ 2423-remove-letter-to-equalize-frequency/2423-remove-letter-to-equalize-frequency.cpp
@@ -0,0 +1,46 @@
+// class Solution {
+// public:
+//     bool equalFrequency(string word) {
+//         unordered_set<int> freqSet;
+//         vector<int> charToFreq(26, 0);
+        
+//         for (auto c: word) charToFreq[c - 'a']++;
+//         for (auto freq: charToFreq) 
+//             if (freq) 
+//                 freqSet.insert(freq);
+        
+//         int minFreq = INT_MAX, maxFreq = INT_MIN;
+//         for (auto freq: freqSet) {
+//             minFreq = min(minFreq, freq);
+//             maxFreq = max(maxFreq, freq);
+//         }
+//         return (maxFreq - minFreq) <= freqSet.size() == 1? 0: 1;
+//     }
+// };
+
+
+class Solution {
+public:
+     bool equalFrequency(string word) {
+        for (int i = 0; i < word.size(); i++) {
+          map<char,int> mp;
+
+          for(int j = 0; j < word.size(); j++) {
+              if (j == i)
+                  continue;
+              else
+                  mp[word[j]]++;
+          }
+
+          set<int> s;
+          for(auto it:mp)
+              s.insert(it.second);
+
+          if (s.size() == 1)
+              return true;
+
+        }
+
+        return false;
+	}
+};
