62f8996d05935324aee9abae88c2d77f19d6f1c4
MuhtasimTanmoy <taaanmo0oy@gmail.com>
2022-11-03 07:51:54 +0600

    Complete design pattern

--- 2413-smallest-even-multiple/2413-smallest-even-multiple.cpp
+++
@@ -1,6 +0,0 @@
-class Solution {
-public:
-    int smallestEvenMultiple(int n) {
-        return (n % 2 == 0) ? n: 2 * n;
-    }
-};
--- 2427-number-of-common-factors/2427-number-of-common-factors.cpp
+++
@@ -1,9 +0,0 @@
-class Solution {
-public:
-    int commonFactors(int a, int b) {
-        int res = 1;
-        for (int n = 2; n <= gcd(a, b); ++n)
-            res += a % n == 0 && b % n == 0;
-        return res;
-    }
-};
