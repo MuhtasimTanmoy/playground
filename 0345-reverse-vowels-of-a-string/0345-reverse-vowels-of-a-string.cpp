ac1411db4f8f11caeb3779ce9f7c23749286340e
Muhtasim Ulfat tanmoy <mtanmoy5086@gmail.com>
2022-11-04 08:20:26 +0600

    Time: 13 ms (62.90%), Space: 8.1 MB (27.79%) - LeetHub

--- 0345-reverse-vowels-of-a-string/0345-reverse-vowels-of-a-string.cpp
+++ 0345-reverse-vowels-of-a-string/0345-reverse-vowels-of-a-string.cpp
@@ -2,19 +2,16 @@ class Solution {
 public:
     string reverseVowels(string s) {
         int left = 0, right = s.size() - 1;
-        
         unordered_set<char> vowelSet = {'a', 'e', 'i', 'o', 'u', 'A', 'E', 'I', 'O', 'U'};
         auto isVowel = [&](char c) { return vowelSet.count(c); };
-        
-        while(left < right) {
-            auto leftChar = s[left], rightChar = s[right];
-            if (isVowel(leftChar) && isVowel(rightChar)) swap(s[left++], s[right--]);
-            else if (isVowel(leftChar) || isVowel(rightChar)) {
-                if (isVowel(leftChar)) right--;
-                if (isVowel(rightChar)) left++;
-            } else right--, left++;
+        while (left < right) {
+            auto isleftCharVowel = isVowel(s[left]), isRightCharVowel = isVowel(s[right]);
+            if (isleftCharVowel && isRightCharVowel) swap(s[left++], s[right--]);
+            else {
+                if (!isleftCharVowel) left++;
+                if (!isRightCharVowel) right--;
+            }
         }
-        
         return s;
     }
 };
