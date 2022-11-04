f4eb8189c7ff1b5bcb05a28bc3c2d3f05e46edf8
MuhtasimTanmoy <taaanmo0oy@gmail.com>
2022-11-04 09:59:53 +0600

    Update all solutions

--- 1913-maximum-product-difference-between-two-pairs/1913-maximum-product-difference-between-two-pairs.cpp
+++
@@ -1,8 +0,0 @@
-class Solution {
-public:
-    int maxProductDifference(vector<int>& nums) {
-        sort(nums.begin(), nums.end());
-        int n = size(nums) - 1;
-        return nums[n] * nums[n-1] - nums[0] * nums[1];
-    }
-};
