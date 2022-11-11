1425a4738efcd3ef12ef787fcba77a01dcffb3fd
Muhtasim Ulfat tanmoy <mtanmoy5086@gmail.com>
2022-11-11 13:43:14 +0600

    Time: 4 ms (62.72%), Space: 6.5 MB (43.04%) - LeetHub

--- 1544-make-the-string-great/1544-make-the-string-great.cpp
+++ 1544-make-the-string-great/1544-make-the-string-great.cpp
@@ -1,15 +1,15 @@
 class Solution {
 public:
     string makeGood(string s) {
-        int dif='a'-'A';
-        stack<char> st;
-        st.push(s[0]);
-        for(int i=1; i < s.length(); i++) {
-            if(!st.empty() and abs(st.top() - s[i]) == dif)  st.pop();
-            else  st.push(s[i]);
+        int dif = 'a' - 'A';
+        stack<char> st; st.push(s[0]);
+        for (int i = 1; i < s.length(); i++) {
+            if (st.size() and abs(st.top() - s[i]) == dif) st.pop();
+            else st.push(s[i]);
         }
-        string ans="";
-        while (!st.empty()) {
+        
+        string ans = "";
+        while (st.size()) {
           ans += st.top();
           st.pop();
         }
