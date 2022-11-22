149c1f8f6eab43e41eac57edc4e1688b2130c78e
Muhtasim Ulfat tanmoy <mtanmoy5086@gmail.com>
2022-11-23 01:43:50 +0600

    Time: 12 ms (39.94%), Space: 7 MB (28.75%) - LeetHub

--- 1758-minimum-changes-to-make-alternating-binary-string/1758-minimum-changes-to-make-alternating-binary-string.cpp
+++ 1758-minimum-changes-to-make-alternating-binary-string/1758-minimum-changes-to-make-alternating-binary-string.cpp
@@ -1,13 +1,21 @@
 class Solution {
 public:
+//     int minOperations(string s) {
+//         int zeroDiff = 0, oneDiff = 0;
+//         int zeroToken = '0', oneToken = '1';
+//         for (auto c: s) {
+//             if (c != zeroToken) zeroDiff++;
+//             if (c != oneToken) oneDiff++;
+//             swap(zeroToken, oneToken);
+//         }
+//         return min(zeroDiff, oneDiff);
+//     }
+    
     int minOperations(string s) {
-        int zeroDiff = 0, oneDiff = 0;
-        int zeroToken = '0', oneToken = '1';
-        for (auto c: s) {
-            if (c != zeroToken) zeroDiff++;
-            if (c != oneToken) oneDiff++;
-            swap(zeroToken, oneToken);
-        }
-        return min(zeroDiff, oneDiff);
-    }
+        int res = 0, sz = s.length();
+        for (int i = 0; i < sz; ++i)
+            res += s[i] - '0' != i % 2;
+        return min(res, sz - res);
+    } 
 };
+
