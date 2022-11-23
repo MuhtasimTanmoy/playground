f7486a445aa26e02d7b1849392d2742ddc7d265c
Muhtasim Ulfat tanmoy <mtanmoy5086@gmail.com>
2022-11-23 21:54:20 +0600

    Time: 3 ms (55.31%), Space: 6.3 MB (55.05%) - LeetHub

--- 1614-maximum-nesting-depth-of-the-parentheses/1614-maximum-nesting-depth-of-the-parentheses.cpp
+++ 1614-maximum-nesting-depth-of-the-parentheses/1614-maximum-nesting-depth-of-the-parentheses.cpp
@@ -1,17 +1,11 @@
 class Solution {
 public:
     int maxDepth(string s) {
-        vector<char> stk;
         int counter = 0, res = 0;
         for (auto c: s) 
-            if (c == ')') {
-                counter--;
-                stk.pop_back();
-            } else if (c == '(') {
-                stk.push_back(c);
-                counter++;
-                res = max(counter, res);
-            }
+            if (c == ')') counter--;
+            else if (c == '(')
+                res = max(++counter, res);
         return res;
     }
 };
