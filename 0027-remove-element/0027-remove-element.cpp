9a318fa789cb890af52a214e8aa474e2aa333a14
Muhtasim Ulfat tanmoy <mtanmoy5086@gmail.com>
2022-11-29 05:26:51 +0600

    Time: 3 ms (70.18%), Space: 8.6 MB (92.12%) - LeetHub

---
+++ 0027-remove-element/0027-remove-element.cpp
@@ -0,0 +1,26 @@
+/*
+class Solution {
+public:
+    int removeElement(vector<int>& nums, int val) {
+        int left = 0;
+        for (int i = 0; i < nums.size(); i++) {
+            nums[left] = nums[i];
+            if (nums[i] != val) left++;
+        }
+        return left;
+    }
+};
+*/
+
+class Solution {
+public:
+    int removeElement(vector<int>& nums, int val) {
+        int left = 0, right = nums.size();
+        while (left < right) 
+            if (nums[left] == val) {
+                nums[left] = nums[right-1];
+                right--;
+            } else left++;
+        return right;
+    }
+};
