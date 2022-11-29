fd3e1bcec869fb1bfb836d451497fd048c5edf32
Muhtasim Ulfat tanmoy <mtanmoy5086@gmail.com>
2022-11-29 16:00:25 +0600

    Time: 692 ms (11.90%), Space: 96.9 MB (93.34%) - LeetHub

--- 0380-insert-delete-getrandom-o1/0380-insert-delete-getrandom-o1.cpp
+++ 0380-insert-delete-getrandom-o1/0380-insert-delete-getrandom-o1.cpp
@@ -1,36 +1,31 @@
 class RandomizedSet {
+    unordered_map<int, int> bag;
+    vector<int> data;
 public:
-    set<int> st;
-    RandomizedSet() {
+    RandomizedSet() {}
+    
+    bool insert(int val) {
+        if (bag.count(val) == 0) {
+            bag[val] = data.size();
+            data.push_back(val);
+            return true;
+        } else return false;
+    }
+    
+    bool remove(int val) {
+        if (bag.count(val) == 0) return false;
         
+        auto currIndexToRemove = bag[val];
+        data[currIndexToRemove] = data.back();
+        bag[data.back()] = currIndexToRemove;
+        
+        data.pop_back();
+        bag.erase(val);
+        return true;
     }
     
-    bool insert(int val) 
-    {
-        if(!st.count(val))                                           // if val is not present in the set then put it into the set and then return true
-        {
-            st.insert(val);
-            return true;
-        }
-        return false;
-    }
-    
-    bool remove(int val) 
-    {
-        if(st.count(val))                                            // if val is present in set then erase it from set and return true
-        {
-            st.erase(val);
-            return true;
-        }
-        return false;
-    }
-    
-    int getRandom() 
-    {
-        if(st.size()!=0)                                             // when stack is not empty only then we can return something
-        {
-            return *next(st.begin(),rand()%st.size());               // it will select randomly next element from set
-        }
-        return 0;
+    int getRandom() {
+        auto index = rand() % data.size();
+        return data[index];
     }
 };
