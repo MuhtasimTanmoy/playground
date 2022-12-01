db77a8e8762a16fd1356b166115b2fd93473ef16
Muhtasim Ulfat tanmoy <mtanmoy5086@gmail.com>
2022-12-01 22:46:19 +0600

    Time: 134 ms (20.48%), Space: 9.2 MB (75.71%) - LeetHub

--- 1419-minimum-number-of-frogs-croaking/1419-minimum-number-of-frogs-croaking.cpp
+++ 1419-minimum-number-of-frogs-croaking/1419-minimum-number-of-frogs-croaking.cpp
@@ -35,10 +35,8 @@ public:
         for (auto c: croakOfFrogs) {
             auto index = string("croak").find(c);
             counter[index]++;
-            if (index == 0) {
-                frog++;
-                maxFrog = max(maxFrog, frog);
-            } else if (--counter[index - 1] < 0) return -1;
+            if (index == 0) maxFrog = max(maxFrog, ++frog);
+            else if (--counter[index - 1] < 0) return -1;
             else if (index == 4) frog--;
         }
         return frog == 0 ? maxFrog: -1;
