bffc8088c1b750fdadf40e2fc7d8d7449d497b87
Muhtasim Ulfat tanmoy <mtanmoy5086@gmail.com>
2022-12-01 22:45:33 +0600

    Time: 136 ms (19.05%), Space: 9.2 MB (40.16%) - LeetHub

---
+++ 1419-minimum-number-of-frogs-croaking/1419-minimum-number-of-frogs-croaking.cpp
@@ -0,0 +1,46 @@
+/*
+class Solution {
+public:
+    int minNumberOfFrogs(string croakOfFrogs) {        
+        int curr = 0;
+        unordered_map<char, vector<int>> mapper;
+        for (int i = 0; i < croakOfFrogs.size(); i++) {
+            auto elem = croakOfFrogs[i];
+            mapper[elem].push_back(i);
+        }
+        
+        auto croak = {'r', 'o', 'a', 'k'};
+        auto prev = mapper['c'];
+        int res = INT_MAX;
+        for (auto c: croak) {
+            auto now = mapper[c];
+            int count = 0;
+            for (int i = 0; i < min(prev.size(), now.size()); i++) 
+                if (prev[i] < now[i]) 
+                    count++;
+
+            res = min(res, count);
+            prev = now;
+        }
+        return res;
+    }
+};
+*/
+
+class Solution {
+public:
+    int minNumberOfFrogs(string croakOfFrogs) {
+        vector<int> counter(5, 0);
+        int frog = 0, maxFrog = 0;
+        for (auto c: croakOfFrogs) {
+            auto index = string("croak").find(c);
+            counter[index]++;
+            if (index == 0) {
+                frog++;
+                maxFrog = max(maxFrog, frog);
+            } else if (--counter[index - 1] < 0) return -1;
+            else if (index == 4) frog--;
+        }
+        return frog == 0 ? maxFrog: -1;
+    }
+};
