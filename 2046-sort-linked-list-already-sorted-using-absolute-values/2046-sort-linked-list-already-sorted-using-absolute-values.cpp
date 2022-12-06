083071d62ba45393de791b4c66453873fa3f0f9a
Muhtasim Ulfat tanmoy <mtanmoy5086@gmail.com>
2022-12-07 02:54:47 +0600

    Time: 305 ms (98.63%), Space: 92.8 MB (94.52%) - LeetHub

---
+++ 2046-sort-linked-list-already-sorted-using-absolute-values/2046-sort-linked-list-already-sorted-using-absolute-values.cpp
@@ -0,0 +1,42 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution {
+public:
+    // ListNode* sortLinkedList(ListNode* head) {
+//         ListNode* prev = NULL, *ans = head;
+//         for (auto itr = head; itr;  ) {
+//             auto next = itr->next;
+//             if (prev && itr->val < 0) {
+//                 prev->next = itr->next;
+//                 itr->next = ans;
+//                 ans = itr;
+//             }
+//             prev = itr;
+//             itr = next;
+//         }
+//         return ans;
+//     }
+    ListNode* sortLinkedList(ListNode* head) {
+        for (ListNode * p = head, 
+             *pp = nullptr; 
+             p != nullptr; 
+             p = p->next) {
+                if (p->val < 0 && pp != nullptr) {
+                    pp->next = p->next;
+                    p->next = head;
+                    head = p;
+                    p = pp;
+                }
+                pp = p;
+        }
+        return head;
+    }
+};
