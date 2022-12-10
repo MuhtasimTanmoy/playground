7c9c69afb4476eb752fedf531f4668b6e9f19c23
Muhtasim Ulfat tanmoy <mtanmoy5086@gmail.com>
2022-12-11 01:48:29 +0600

    Time: 0 ms (100.00%), Space: 6.7 MB (78.41%) - LeetHub

--- 2490-circular-sentence/2490-circular-sentence.cpp
+++ 2490-circular-sentence/2490-circular-sentence.cpp
@@ -1,13 +1,23 @@
+// class Solution {
+// public:
+//     bool isCircularSentence(string sentence) {
+//         stringstream ss(sentence);
+//         string now, prev = "", firstCache;
+//         while (ss >> now) {
+//             if (prev.size()) { if (prev.back() != now.front()) return false; }
+//             else firstCache = now;
+//             prev = now;
+//         }
+//         return firstCache.front() == now.back();
+//     }
+// };
+
 class Solution {
-public:
-    bool isCircularSentence(string sentence) {
-        stringstream ss(sentence);
-        string now, prev = "", firstCache;
-        while (ss >> now) {
-            if (prev.size()) { if (prev.back() != now.front()) return false; }
-            else firstCache = now;
-            prev = now;
-        }
-        return firstCache.front() == now.back();
+    public:
+    bool isCircularSentence(string s) {
+        for (int i = 0; i < s.size(); ++i) 
+            if (s[i] == ' ' && s[i - 1] != s[i + 1])
+                return false;
+        return s.front() == s.back();
     }
 };
