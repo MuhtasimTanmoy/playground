b78d5bd00cef0ee25ac7da558c8dc3a807c3b4fd
Muhtasim Ulfat tanmoy <mtanmoy5086@gmail.com>
2022-12-30 23:50:29 +0600

    Time: 117 ms (99.86%), Space: 75.2 MB (96.14%) - LeetHub

--- 0123-best-time-to-buy-and-sell-stock-iii/0123-best-time-to-buy-and-sell-stock-iii.cpp
+++ 0123-best-time-to-buy-and-sell-stock-iii/0123-best-time-to-buy-and-sell-stock-iii.cpp
@@ -1,3 +1,4 @@
+/*
 class Solution {
 public:
     int maxProfit(vector<int>& prices) {
@@ -15,9 +16,22 @@ public:
         }
         
         int res = INT_MIN;
-        for (int i = 0; i < prices.size(); i++) 
-            res = max(res, l[i] + r[i+1]);
-        
+        for (int i = 0; i < prices.size(); i++) res = max(res, l[i] + r[i+1]);
         return res;
     }
+};
+*/
+class Solution {
+  public:
+  int maxProfit(vector<int>& prices) {
+    int t1Cost = INT_MAX, t2Cost = INT_MAX;
+    int t1Profit = 0, t2Profit = 0;
+    for (int price : prices) {
+        t1Cost = min(t1Cost, price);
+        t1Profit = max(t1Profit, price - t1Cost);
+        t2Cost = min(t2Cost, price - t1Profit);
+        t2Profit = max(t2Profit, price - t2Cost);
+    }
+    return t2Profit;
+  }
 };
