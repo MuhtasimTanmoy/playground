cbf05c8d384c9e13abe273b74a2ebd41b37c4f35
Muhtasim Ulfat tanmoy <mtanmoy5086@gmail.com>
2023-01-01 11:55:17 +0600

    Time: 0 ms (100.00%), Space: 6.4 MB (53.35%) - LeetHub

--- 0290-word-pattern/0290-word-pattern.cpp
+++ 0290-word-pattern/0290-word-pattern.cpp
@@ -4,16 +4,16 @@ public:
         unordered_map<string, char> mapper;
         unordered_map<char, string> revMapper;
         stringstream ss(s); string token; int matchIndex = 0;
-        while (ss >> token) {
-            if (matchIndex >= pattern.size()) return false;
+        
+        for (string token; ss >> token; matchIndex++) {
+            if (matchIndex ==  pattern.size()) return false;
             auto charToken = pattern[matchIndex];
-            if (mapper.count(token) || revMapper.count(charToken)) {
+            if (mapper.count(token) 
+                || revMapper.count(charToken)) {
                 if (mapper[token] != charToken || revMapper[charToken] != token) return false; 
             } else {
-                mapper[token] = charToken;
-                revMapper[charToken] = token;
+                mapper[token] = charToken; revMapper[charToken] = token;
             }
-            matchIndex++;
         }
         return matchIndex == pattern.size();
     }
