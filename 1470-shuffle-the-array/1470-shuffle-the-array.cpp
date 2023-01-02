df12e62d1e37873e6cfc2c91b7d1f2a064ca6f70
Muhtasim Ulfat tanmoy <mtanmoy5086@gmail.com>
2023-01-02 15:29:22 +0600

    Time: 3 ms (95.91%), Space: 9.7 MB (61.72%) - LeetHub

---
+++ 1470-shuffle-the-array/1470-shuffle-the-array.cpp
@@ -0,0 +1,11 @@
+class Solution {
+public:
+    vector<int> shuffle(vector<int>& nums, int n) {
+        vector<int> res;
+        for (int i = 0; i < n; i ++) {
+            res.push_back(nums[i]);
+            res.push_back(nums[n + i]);
+        }
+        return res;
+    }
+};
