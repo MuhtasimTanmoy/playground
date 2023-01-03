ea4451aa8e741d739d891a3d1d41b9e2815bdb63
Muhtasim Ulfat tanmoy <mtanmoy5086@gmail.com>
2023-01-03 10:25:37 +0600

    Time: 1405 ms (5.18%), Space: 494.9 MB (5.80%) - LeetHub

--- 0944-delete-columns-to-make-sorted/0944-delete-columns-to-make-sorted.cpp
+++ 0944-delete-columns-to-make-sorted/0944-delete-columns-to-make-sorted.cpp
@@ -15,3 +15,15 @@ public:
         return del;
     }
 };
+
+// int minDeletionSize(vector<string>& A, int res = 0) {
+//     if (A.empty()) return res;
+//     for (auto c = 0; c < A[0].size(); ++c)
+//         for (auto i = 1; i < A.size(); ++i) {
+//             if (A[i - 1][c] > A[i][c]) {
+//                 ++res;
+//                 break;
+//             }
+//         }
+//     return res;
+// }
