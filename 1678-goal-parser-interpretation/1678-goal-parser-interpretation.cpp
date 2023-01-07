19f3ee41e7000e730f2d5507f6e6a8c4d1327238
Muhtasim Ulfat tanmoy <mtanmoy5086@gmail.com>
2023-01-07 18:30:25 +0600

    Time: 0 ms (100.00%), Space: 6.2 MB (59.90%) - LeetHub

--- 1678-goal-parser-interpretation/1678-goal-parser-interpretation.cpp
+++ 1678-goal-parser-interpretation/1678-goal-parser-interpretation.cpp
@@ -3,9 +3,10 @@ public:
     string interpret(string command) {
         string res = "";
         for (int i = 0; i < command.size(); i++) {
-            if (i && command[i] == ')' && command[i-1] == '(') res += 'o';
-            if (command[i] == '(' || command[i] == ')') continue;
-            res += command[i];
+            auto curr = command[i];
+            if (i && curr == ')' && command[i-1] == '(') res += 'o';
+            if (curr == '(' || curr == ')') continue;
+            res += curr;
         }
         return res;
     }
