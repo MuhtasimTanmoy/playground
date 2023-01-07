6b361e6931d525ff670a66cdc4465a9c5183ab49
Muhtasim Ulfat tanmoy <mtanmoy5086@gmail.com>
2023-01-07 20:14:59 +0600

    Time: 75 ms (81.82%), Space: 3.5 MB (27.27%) - LeetHub

--- 1605-find-valid-matrix-given-row-and-column-sums/1605-find-valid-matrix-given-row-and-column-sums.rs
+++ 1605-find-valid-matrix-given-row-and-column-sums/1605-find-valid-matrix-given-row-and-column-sums.rs
@@ -1,7 +1,6 @@
 impl Solution {
     pub fn restore_matrix(rs: Vec<i32>, cs: Vec<i32>) -> Vec<Vec<i32>> {
-        let mut row_sum = rs;
-        let mut col_sum = cs;
+        let (mut row_sum, mut col_sum) = (rs, cs);
         let mut res: Vec<Vec<i32>> = vec![vec![0; col_sum.len()]; row_sum.len()];
         for i in 0..res.len() {
             for j in 0..res[0].len() {
