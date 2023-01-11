3915a6e4f0bd3adf85cf30299adf532ec7f4072c
Muhtasim Ulfat tanmoy <mtanmoy5086@gmail.com>
2023-01-12 00:47:47 +0600

    Time: 274 ms (29.53%), Space: 48.5 MB (79.66%) - LeetHub

--- 2517-maximum-tastiness-of-candy-basket/2517-maximum-tastiness-of-candy-basket.cpp
+++ 2517-maximum-tastiness-of-candy-basket/2517-maximum-tastiness-of-candy-basket.cpp
@@ -1,19 +1,18 @@
 class Solution {
-    bool check(int tar, vector<int>& price, int k) {
-        int count = 1;
-        for (int i = 1, j = 0; i < price.size(); i++)
-            if (price[i] - price[j] >= tar)
-                j = i, count++;
-        return count >= k;
-    }
 public:
     int maximumTastiness(vector<int>& price, int k) {
         sort(price.begin(), price.end());
+        auto check = [&](int target) {
+            int count = 1;
+            for (int i = 1, j = 0; i < price.size(); i++) 
+                if (price[i] - price[j] >= target) 
+                    j = i, count++;
+            return count >= k;
+        };
         auto left = 0, right = (int) price.back() - price.front() + 1;
         while (left < right) {
             auto mid = left + (right - left) / 2;
-            if (check(mid, price, k)) left = mid + 1; 
-            else right = mid;
+            if (check(mid)) left = mid + 1; else right = mid;
         }
         return left - 1;
     }
