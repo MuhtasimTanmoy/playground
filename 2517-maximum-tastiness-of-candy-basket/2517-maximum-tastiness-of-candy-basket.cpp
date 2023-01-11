453a75745a93a43429d04656a91df509f1e7556a
Muhtasim Ulfat tanmoy <mtanmoy5086@gmail.com>
2023-01-12 00:39:22 +0600

    Time: 263 ms (37.06%), Space: 48.6 MB (41.52%) - LeetHub

---
+++ 2517-maximum-tastiness-of-candy-basket/2517-maximum-tastiness-of-candy-basket.cpp
@@ -0,0 +1,21 @@
+class Solution {
+    bool check(int tar, vector<int>& price, int k) {
+        int count = 1;
+        for (int i = 1, j = 0; i < price.size(); i++)
+            if (price[i] - price[j] >= tar)
+                j = i, count++;
+        return count >= k;
+    }
+public:
+    int maximumTastiness(vector<int>& price, int k) {
+        sort(price.begin(), price.end());
+        auto left = 0, right = (int) 1e9;
+        while (left < right) {
+            auto mid = left + (right - left) / 2;
+            // cout<<mid<<endl;
+            if (check(mid, price, k)) left = mid + 1; 
+            else right = mid;
+        }
+        return left - 1;
+    }
+};
