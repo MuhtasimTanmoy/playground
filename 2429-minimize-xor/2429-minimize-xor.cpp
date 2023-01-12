a2914ec9c3bce798cf82ac0cb72be77d9fb5a072
Muhtasim Ulfat tanmoy <mtanmoy5086@gmail.com>
2023-01-12 12:29:07 +0600

    Time: 0 ms (100.00%), Space: 5.8 MB (83.78%) - LeetHub

--- 2429-minimize-xor/2429-minimize-xor.cpp
+++ 2429-minimize-xor/2429-minimize-xor.cpp
@@ -5,30 +5,22 @@ public:
     int minimizeXor(int num1, int num2) {
         auto x = num1;
         auto toAddInX = __builtin_popcount(num2) - __builtin_popcount(x);
-        cout<<toAddInX<<endl;
-        
-        if (toAddInX > 0) {
-            int traverse = 0;
-            while (toAddInX) {
-                if (x & (1 << traverse)) {} else {
+        int traverse = 0;
+        while (toAddInX != 0) {
+            auto isOne = x & (1 << traverse);
+            if (toAddInX > 0) {
+                if (!isOne) {
                     x |= 1 << traverse;
                     toAddInX--;
                 }
-                traverse++;
-            }
-        } 
-        
-        if (toAddInX < 0) {
-            int traverse = 0;
-            while (toAddInX != 0) {
-                if (x & (1 << traverse)) {
+            } else {
+                if (isOne) {
                     x ^= 1 << traverse;
                     toAddInX++;
                 }
-                traverse++;
             }
-        } 
-        
+            traverse++;
+        }
         return x;
     }
 };
