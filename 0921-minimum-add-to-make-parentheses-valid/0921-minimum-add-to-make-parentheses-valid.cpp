a5ec7b626dea9b12796a1df9257711850ba95604
Muhtasim Ulfat tanmoy <mtanmoy5086@gmail.com>
2023-01-12 06:05:21 +0600

    Time: 4 ms (42.41%), Space: 6.2 MB (90.58%) - LeetHub

---
+++ 0921-minimum-add-to-make-parentheses-valid/0921-minimum-add-to-make-parentheses-valid.cpp
@@ -0,0 +1,10 @@
+class Solution {
+public:
+    int minAddToMakeValid(string s, int res = 0) {
+        string stk;
+        for (auto c: s) 
+            if (c == ')') { if (stk.empty()) res++; else stk.pop_back(); }
+            else stk += c;
+        return res + stk.size();
+    }
+};
