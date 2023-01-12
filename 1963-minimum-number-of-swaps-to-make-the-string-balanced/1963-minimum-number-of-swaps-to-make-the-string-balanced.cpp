d592e9d8de9db202aadd1f2cb46bc759c391cd04
Muhtasim Ulfat tanmoy <mtanmoy5086@gmail.com>
2023-01-12 11:02:21 +0600

    Time: 91 ms (96.90%), Space: 31.2 MB (59.39%) - LeetHub

---
+++ 1963-minimum-number-of-swaps-to-make-the-string-balanced/1963-minimum-number-of-swaps-to-make-the-string-balanced.cpp
@@ -0,0 +1,11 @@
+class Solution {
+public:
+    int minSwaps(string s) {
+        int size = 0;
+        for (char &ch : s) {
+            if (ch == '[') size++;
+            else if(size > 0) size--;
+        }
+        return (size + 1)/ 2;
+    }
+};
