08f3a03c10a4a0aee1e9b0b994d6a95020c6285b
Muhtasim Ulfat tanmoy <mtanmoy5086@gmail.com>
2023-01-12 12:34:03 +0600

    Time: 0 ms (100.00%), Space: 5.9 MB (83.78%) - LeetHub

--- 2429-minimize-xor/2429-minimize-xor.cpp
+++ 2429-minimize-xor/2429-minimize-xor.cpp
@@ -1,12 +1,11 @@
 class Solution {
 public:
-    int minimizeXor(int num1, int num2, int traverse = 0) {
+    int minimizeXor(int num1, int num2, int traverse = -1) {
         auto toAddInX = __builtin_popcount(num2) - __builtin_popcount(num1);
         while (toAddInX != 0) {
-            auto isOne = num1 & (1 << traverse);
+            auto isOne = num1 & (1 << ++traverse);
             if (toAddInX > 0 && !isOne) num1 |= 1 << traverse, toAddInX--;
             if (toAddInX < 0 && isOne) num1 ^= 1 << traverse, toAddInX++;
-            traverse++;
         }
         return num1;
     }
