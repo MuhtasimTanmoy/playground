1c314275a20d6b57cbf7865c4d3947251babd7b2
Muhtasim Ulfat tanmoy <mtanmoy5086@gmail.com>
2023-01-15 12:05:33 +0600

    Time: 347 ms (89.91%), Space: 134.6 MB (76.09%) - LeetHub

--- 2471-minimum-number-of-operations-to-sort-a-binary-tree-by-level/2471-minimum-number-of-operations-to-sort-a-binary-tree-by-level.cpp
+++ 2471-minimum-number-of-operations-to-sort-a-binary-tree-by-level/2471-minimum-number-of-operations-to-sort-a-binary-tree-by-level.cpp
@@ -14,7 +14,7 @@ public:
     int minimumOperations(TreeNode* root) {
         int res = 0;
         vector<TreeNode*> q{root};
-        while(!q.empty()) {
+        while(q.size()) {
             vector<TreeNode*> q1;
             vector<int> vals, ids(q.size());
             for (auto n: q) {
@@ -25,7 +25,7 @@ public:
             
             iota(begin(ids), end(ids), 0);
             sort(begin(ids), end(ids), [&](int i, int j) { return vals[i] < vals[j]; });
-            for (int i = 0; i < ids.size(); ++i)
+            for (int i = 0; i < ids.size(); ++i) 
                 for (; ids[i] != i; ++res)
                     swap(ids[i], ids[ids[i]]);
             swap(q, q1);
