86fa05b841e2bf8187370e0bbb4150618f44c801
MuhtasimTanmoy <taaanmo0oy@gmail.com>
2023-01-17 23:43:26 +0600

    Update

--- 0921-minimum-add-to-make-parentheses-valid/0921-minimum-add-to-make-parentheses-valid.cpp
+++
@@ -1,9 +0,0 @@
-class Solution {
-public:
-    int minAddToMakeValid(string s, int res = 0, string stk = "") {
-        for (auto c: s) 
-            if (c == ')') { if (stk.empty()) res++; else stk.pop_back(); }
-            else stk += c;
-        return res + stk.size();
-    }
-};
--- 2390-removing-stars-from-a-string/2390-removing-stars-from-a-string.cpp
+++
@@ -1,9 +0,0 @@
-class Solution {
-public:
-    string removeStars(string s) {
-        string stk;
-        for (auto c: s) 
-            if (c == '*') stk.pop_back(); else stk += c;
-        return stk;
-    }
-};
