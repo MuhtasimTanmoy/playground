c362e0b06cb91f28e10d929fd0c913cfc1bf37b0
Muhtasim Ulfat tanmoy <mtanmoy5086@gmail.com>
2023-01-18 14:39:05 +0600

    Time: 0 ms (100.00%), Space: 5.9 MB (52.84%) - LeetHub

---
+++ 0371-sum-of-two-integers/0371-sum-of-two-integers.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    int getSum(int a, int b) {
+        long mask = 0xFFFFFFFF;
+        
+        while (b != 0) {
+            int answer = (a ^ b) & mask;
+            int carry = ((a & b) & mask) << 1;
+            a = answer;
+            b = carry;
+        }
+         
+        return a < INT_MAX ? a : ~(a ^ mask);
+    }
+};
