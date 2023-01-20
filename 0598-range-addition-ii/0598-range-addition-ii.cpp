795079ec7b21e8553ce938a03453f2ae4230de12
Muhtasim Ulfat tanmoy <mtanmoy5086@gmail.com>
2023-01-20 16:07:17 +0600

    Time: 18 ms (35.22%), Space: 16.6 MB (12.53%) - LeetHub

---
+++ 0598-range-addition-ii/0598-range-addition-ii.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    int maxCount(int m, int n, vector<vector<int>>& ops, int res = 0) {
+        vector<int> rows(n + 1, 0), cols(m + 1, 0);
+        for (auto op: ops) {
+            auto x = op[0], y = op[1];
+            rows[0]++; rows[y]--;
+            cols[0]++; cols[x]--;
+        }
+        
+        int inRow = 0, inCol = 0;
+        for (auto i = 0, calc = 0; i < rows.size() - 1; i++) {
+            calc += rows[i];
+            res = max(res, calc);
+            if (res == calc) inRow++;
+        }
+        for (auto i = 0, calc = 0; i < cols.size() - 1; i++) {
+            calc += cols[i];
+            res = max(res, calc);
+            if (res == calc) inCol++;
+        }
+        return inRow * inCol;
+    }
+};
