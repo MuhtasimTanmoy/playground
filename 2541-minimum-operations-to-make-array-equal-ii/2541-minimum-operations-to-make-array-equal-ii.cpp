130cf2901b92ecc5bb20ae90e8c4d5299c972c3d
T <mtanmoy5086@gmail.com>
2023-01-22 13:41:00 +0600

    Time: 191 ms (40.00%), Space: 72.3 MB (30.00%) - LeetHub

--- 2541-minimum-operations-to-make-array-equal-ii/2541-minimum-operations-to-make-array-equal-ii.cpp
+++ 2541-minimum-operations-to-make-array-equal-ii/2541-minimum-operations-to-make-array-equal-ii.cpp
@@ -18,20 +18,15 @@ public:
 
 class Solution {
 public:
-    long long minOperations(vector<int>& nums1, 
-                            vector<int>& nums2, 
-                            int k,
-                            long long absSum = 0,
-                            long long sum = 0) {        
+    long long minOperations(vector<int>& nums1, vector<int>& nums2, int k,
+                            long long absSum = 0, long long sum = 0) {        
         for (int i = 0; i < nums1.size(); i++) {
             auto diff = nums2[i] - nums1[i];
-            if (k == 0) {
-                if (diff) return -1;
-                continue;
-            }
-            if (diff % k != 0) return -1;
-            sum += diff;
-            absSum += abs(diff) / k;
+            if (k) {
+                if (diff % k != 0) return -1;
+                sum += diff;
+                absSum += abs(diff) / k;
+            } else if (diff) return -1;
         }
         return sum == 0 ? absSum / 2 : -1;
     }
