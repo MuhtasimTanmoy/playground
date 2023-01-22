90d2e060ba645a086dbc40da42fa9b0e7a54853b
T <mtanmoy5086@gmail.com>
2023-01-23 01:13:39 +0600

    Time: 457 ms (16.26%), Space: 86.9 MB (39.86%) - LeetHub

--- 2536-increment-submatrices-by-one/2536-increment-submatrices-by-one.cpp
+++ 2536-increment-submatrices-by-one/2536-increment-submatrices-by-one.cpp
@@ -6,8 +6,8 @@ public:
             auto x = query[0], y = query[1], toX = query[2], toY = query[3];
             res[x][y]++, res[x][toY + 1]--, res[toX + 1][y]--, res[toX + 1][toY + 1]++;
         }
-        for (int i = 0; i < n + 1; i++) 
-            for (int j = 0; j < n + 1; j++) {
+        for (int i = 0; i < n; i++) 
+            for (int j = 0; j < n; j++) {
                 if (i) res[i][j] += res[i-1][j];
                 if (j) res[i][j] += res[i][j-1];
                 if (i && j) res[i][j] -= res[i-1][j-1];
