3a0f2053efffe92572134888bdf6e3bb39aeb534
T <mtanmoy5086@gmail.com>
2023-01-23 10:24:07 +0600

    Time: 224 ms (54.69%), Space: 69.6 MB (19.44%) - LeetHub

--- 0997-find-the-town-judge/0997-find-the-town-judge.cpp
+++ 0997-find-the-town-judge/0997-find-the-town-judge.cpp
@@ -1,14 +1,11 @@
 class Solution {
 public:
     int findJudge(int n, vector<vector<int>>& trust) {
-        vector<int> parent(n, 0);
+        vector<int> childCount(n + 1, 0);
         unordered_set<int> cantBe;
-        for (auto t: trust) {
-            parent[t[1] - 1]++;
-            cantBe.insert(t[0]);
-        }
-        for (int i = 0; i < parent.size(); i++) 
-            if (parent[i] == n - 1 && cantBe.count(i + 1) == 0) return i + 1;
+        for (auto t: trust) childCount[t[1]]++, cantBe.insert(t[0]);
+        for (int i = 1; i < childCount.size(); i++) 
+            if (childCount[i] == n - 1 && !cantBe.count(i)) return i;
         return -1;
     }
 };
