6c21d96996bfcf21cdc6e0e7d11f1ae2dbd0ea7f
T <mtanmoy5086@gmail.com>
2023-01-23 22:49:39 +0600

    Time: 186 ms (71.39%), Space: 40.6 MB (51.65%) - LeetHub

--- 2178-maximum-split-of-positive-even-integers/2178-maximum-split-of-positive-even-integers.cpp
+++ 2178-maximum-split-of-positive-even-integers/2178-maximum-split-of-positive-even-integers.cpp
@@ -1,25 +1,32 @@
 
+// class Solution {
+// public:
+//     vector<long long> maximumEvenSplit(long long finalSum) {
+//         if (finalSum & 1) return {};
+//         vector<long long>  ans;
+//         function<bool(long, long)> go = [&] (auto val, auto sum) {
+//             if (sum == 0) return true;
+//             if (sum < val) return false;
+//             ans.push_back(val);
+//             if (go(val + 2, sum - val)) return true;
+//             ans.pop_back();
+//             return go(val + 2, sum);
+//         };
+//         go(2, finalSum);
+//         return ans;
+//     }
+// };
+
 class Solution {
 public:
     vector<long long> maximumEvenSplit(long long finalSum) {
         if (finalSum & 1) return {};
-        vector<long long>  ans;
-        function<bool(long, long)> go = [&] (auto val, auto sum) {
-            if (sum == 0) return true;
-            if (sum < val) return false;
-            ans.push_back(val);
-            if (go(val + 2, sum - val)) return true;
-            ans.pop_back();
-            return go(val + 2, sum);
-        };
-        go(2, finalSum);
-        return ans;
+        vector<long long> res;
+        for (auto curr = 2; finalSum > 0; curr += 2) {
+            if (finalSum - curr < curr + 2) res.push_back(finalSum);
+            else res.push_back(curr);
+            finalSum -= res.back();
+        }
+        return res;
     }
 };
-
-// class Solution {
-// public:
-//     vector<long long> maximumEvenSplit(long long finalSum) {
-//         if (finalSum & 1) return [}]
-//     }
-// };
