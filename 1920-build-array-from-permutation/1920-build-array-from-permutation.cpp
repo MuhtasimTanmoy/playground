64db164aa17c80a26c57a5421415b9631723ccfe
T <mtanmoy5086@gmail.com>
2023-01-23 15:36:14 +0600

    Time: 21 ms (45.92%), Space: 16.2 MB (82.00%) - LeetHub

--- 1920-build-array-from-permutation/1920-build-array-from-permutation.cpp
+++ 1920-build-array-from-permutation/1920-build-array-from-permutation.cpp
@@ -1,8 +1,24 @@
+/*
+O(n) space
 class Solution {
 public:
     vector<int> buildArray(vector<int>& nums) {
         vector<int> ans(nums.size(), 0);
         for (int i = 0; i < nums.size(); i++) ans[i] = nums[nums[i]];
         return ans;
+    }
+};
+*/
+
+
+class Solution {
+public:
+    vector<int> buildArray(vector<int>& nums) {
+        int n = nums.size();
+        for (int i = 0; i < nums.size(); i++) 
+            nums[i] += n * (nums[nums[i]] % n );
+        for (int i = 0; i < nums.size(); i++) 
+            nums[i] /= n;
+        return nums;
     }
 };
