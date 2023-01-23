dd49db9c9cd168977674e4dddd23534397d958a2
T <mtanmoy5086@gmail.com>
2023-01-23 14:46:40 +0600

    Time: 5 ms (12.50%), Space: 6 MB (81.90%) - LeetHub

--- 1507-reformat-date/1507-reformat-date.cpp
+++ 1507-reformat-date/1507-reformat-date.cpp
@@ -4,24 +4,19 @@ public:
         auto months = {"Jan", "Feb", "Mar", "Apr", "May", "Jun", 
                        "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"};
         stringstream ss(date); string token;
-        int index = 0;
-        while (ss >> token) {
-            switch (index) {
-                case 0:
-                    token.pop_back(); token.pop_back();
-                    if (token.size() == 1) token = "0" + token;
-                    res += token; break;
-                case 1: {
-                    int idx = find(months.begin(),months.end(), token) - months.begin() + 1;
-                    res = to_string(idx) + "-" + res;
-                    if (idx <= 9) res = "0" + res;
-                    break;
-                }
-                case 2:
-                    res = token + "-" + res;
-            }
-            index++;
-        }
+        
+        ss >> token;
+        token.pop_back(); token.pop_back();
+        if (token.size() == 1) token = "0" + token;
+        res += token;
+        
+        ss >> token;
+        int idx = find(months.begin(),months.end(), token) - months.begin() + 1;
+        res = to_string(idx) + "-" + res;
+        if (idx <= 9) res = "0" + res;
+        
+        ss >> token;
+        res = token + "-" + res;
         return res;
     }
 };
