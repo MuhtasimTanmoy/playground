19a5341c0bd0b14fe566f40f3a46a420a44f8cc9
T <mtanmoy5086@gmail.com>
2023-01-23 23:22:02 +0600

    Time: 439 ms (15.71%), Space: 117.6 MB (30.99%) - LeetHub

--- 1775-equal-sum-arrays-with-minimum-number-of-operations/1775-equal-sum-arrays-with-minimum-number-of-operations.cpp
+++ 1775-equal-sum-arrays-with-minimum-number-of-operations/1775-equal-sum-arrays-with-minimum-number-of-operations.cpp
@@ -27,28 +27,23 @@ public:
 //     }
     
     int minOperations(vector<int>& n1, vector<int>& n2) {
-        if (n2.size() * 6 < n1.size() || n1.size() * 6 < n2.size())
-            return -1;
+        if (6 * min(n1.size(), n2.size()) <  max(n1.size(), n2.size()) ) return -1;
         
-        int sum1 = accumulate(begin(n1), end(n1), 0), 
-            sum2 = accumulate(begin(n2), end(n2), 0);
-        
-        if (sum1 > sum2)
-            swap(n1, n2);
+        int sum1 = accumulate(begin(n1), end(n1), 0), sum2 = accumulate(begin(n2), end(n2), 0);
+        if (sum1 > sum2) swap(n1, n2);
         
         priority_queue <int, vector<int>, greater<int>> q1(begin(n1), end(n1));
         priority_queue<int> q2(begin(n2), end(n2));
+        
         int cnt = 0, diff = abs(sum1 - sum2);
-        while (diff > 0) {
+        while (diff > 0 && q1.size() && q2.size()) {
             ++cnt;
-            if (q1.empty() || (!q2.empty() && q2.top() - 1 > 6 - q1.top())) {
-                diff -= q2.top() - 1;
-                q2.pop();
-            } else {
-                diff -= 6 - q1.top();
-                q1.pop();
-            }
+            auto greaterToLess = q2.top() - 1, lessToGreater = 6 - q1.top();
+            if (greaterToLess > lessToGreater) diff -= greaterToLess, q2.pop();
+            else diff -= lessToGreater, q1.pop();
         }
-        return cnt;
+        while (diff > 0 && q1.size()) diff -= 6 - q1.top(), q1.pop(), cnt++;
+        while (diff > 0 && q2.size()) diff -= q2.top() - 1, q2.pop(), cnt++;
+        return diff > 0 ? -1: cnt;
     }
 };
