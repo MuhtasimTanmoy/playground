9554cf96fb83ef0ad752965f97593061bcbaa81d
T <mtanmoy5086@gmail.com>
2023-01-25 14:53:47 +0600

    Time: 80 ms (45.29%), Space: 21.8 MB (32.61%) - LeetHub

--- 0661-image-smoother/0661-image-smoother.cpp
+++ 0661-image-smoother/0661-image-smoother.cpp
@@ -1,7 +1,6 @@
 class Solution {
 public:
     vector<vector<int>> imageSmoother(vector<vector<int>>& img) {
-        vector<vector<int>> res(img.size(), vector<int>(img[0].size(), 0));
         auto shouldGo = [&](int p, int q) {
             if (p < 0 || p >= img.size()) return false;
             if (q < 0 || q >= img[p].size()) return false;
@@ -12,12 +11,13 @@ public:
                 int sum = 0, count = 0;
                 for (int x = -1; x <= 1; x++) 
                     for (int y = -1; y <= 1; y++) 
-                        if (shouldGo(i + x, j + y)) {
-                            sum += img[i + x][j + y];
-                            count++;
-                        }
-            res[i][j] = sum / count;
+                        if (shouldGo(i + x, j + y)) 
+                            sum += img[i + x][j + y] & 0xFF, count++;
+            img[i][j] |= (sum / count) << 8;
         }
-        return res;
+        for (auto i = 0; i < img.size(); i++) 
+            for (auto j = 0; j < img[i].size(); j++) 
+                img[i][j] = img[i][j] >> 8;
+        return img;
     }
 };
