73bb55f0942fa3a5326e96d1a14c201a68fa72ed
T <mtanmoy5086@gmail.com>
2023-01-25 12:30:57 +0600

    Time: 0 ms (100.00%), Space: 5.9 MB (54.18%) - LeetHub

--- 2513-minimize-the-maximum-of-two-arrays/2513-minimize-the-maximum-of-two-arrays.cpp
+++ 2513-minimize-the-maximum-of-two-arrays/2513-minimize-the-maximum-of-two-arrays.cpp
@@ -16,18 +16,18 @@ public:
     int minimizeSet(int d1, int d2, int cnt1, int cnt2) {
         long long ans = INT_MAX;
         long long low = 1, high = INT_MAX;
-        auto common = lcm((long long) d1, d2);
+        auto common = (long long) d1 * d2 / __gcd(d1,d2); // lcm((long long) d1, d2);
         
         while (low <= high) {
             auto mid = ( low + high ) / 2;
-            auto c1 = mid - mid / d1;
-            auto c2 = mid - mid / d2;
-            auto temp = mid - mid / common;
+            auto notDivisibleByd1 = mid - mid / d1;
+            auto notDivisibleByd2 = mid - mid / d2;
+            auto notDivisibleByBoth = mid - mid / common;
             
-            if (c1 >= cnt1 and c2 >= cnt2 and temp >= cnt1 + cnt2) {
-                ans = min(ans, mid);
-                high = mid - 1;
-            }
+            if (notDivisibleByd1 >= cnt1 
+                and notDivisibleByd2 >= cnt2 
+                and notDivisibleByBoth >= cnt1 + cnt2) 
+                    ans = mid, high = mid - 1;
             else low = mid + 1;
         }
         return ans;
