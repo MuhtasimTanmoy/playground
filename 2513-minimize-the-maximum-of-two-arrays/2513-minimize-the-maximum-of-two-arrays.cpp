5993484794959870a581c73e3b81facdacfd7a29
T <mtanmoy5086@gmail.com>
2023-01-25 12:16:09 +0600

    Time: 0 ms (100.00%), Space: 5.9 MB (92.17%) - LeetHub

---
+++ 2513-minimize-the-maximum-of-two-arrays/2513-minimize-the-maximum-of-two-arrays.cpp
@@ -0,0 +1,34 @@
+class Solution {
+public:
+    // int minimizeSet(int divisor1, int divisor2, 
+    //                 int uniqueCnt1, int uniqueCnt2, int start = 0) {
+    //     while (uniqueCnt1 || uniqueCnt2) {
+    //         if (uniqueCnt1 <= uniqueCnt2) 
+    //             swap(uniqueCnt1, uniqueCnt2), swap(divisor1, divisor2);
+    //         start++;
+    //         if (start % divisor1) uniqueCnt1--;
+    //         else if (uniqueCnt2) 
+    //             if (start % divisor2) uniqueCnt2--;
+    //     }
+    //     return start;
+    // }
+    
+    int minimizeSet(int d1, int d2, int cnt1, int cnt2) {
+        long long ans = INT_MAX;
+        long long low = 1, high = INT_MAX;
+        auto common = (long long) d1 * d2 / __gcd(d1,d2);
+        while (low <= high) {
+            long long mid = ( low + high ) / 2;
+            long long c1 = mid - mid / d1;
+            long long c2 = mid - mid / d2;
+            long long temp = mid - mid / common;
+            
+            if (c1 >= cnt1 and c2 >= cnt2 and temp >= cnt1 + cnt2) {
+                ans = min(ans, mid);
+                high = mid - 1;
+            }
+            else low = mid + 1;
+        }
+        return ans;
+    }
+};
