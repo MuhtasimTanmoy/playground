cc71918bd3a23438ed95d05f32a90b4aa1567811
T <mtanmoy5086@gmail.com>
2023-01-25 12:19:01 +0600

    Time: 0 ms (100.00%), Space: 5.9 MB (92.17%) - LeetHub

--- 2513-minimize-the-maximum-of-two-arrays/2513-minimize-the-maximum-of-two-arrays.cpp
+++ 2513-minimize-the-maximum-of-two-arrays/2513-minimize-the-maximum-of-two-arrays.cpp
@@ -17,11 +17,12 @@ public:
         long long ans = INT_MAX;
         long long low = 1, high = INT_MAX;
         auto common = (long long) d1 * d2 / __gcd(d1,d2);
+        
         while (low <= high) {
-            long long mid = ( low + high ) / 2;
-            long long c1 = mid - mid / d1;
-            long long c2 = mid - mid / d2;
-            long long temp = mid - mid / common;
+            auto mid = ( low + high ) / 2;
+            auto c1 = mid - mid / d1;
+            auto c2 = mid - mid / d2;
+            auto temp = mid - mid / common;
             
             if (c1 >= cnt1 and c2 >= cnt2 and temp >= cnt1 + cnt2) {
                 ans = min(ans, mid);
