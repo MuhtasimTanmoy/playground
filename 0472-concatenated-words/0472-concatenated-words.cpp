a549a01270c141e7c713f38ebf8822fba1c448d5
T <mtanmoy5086@gmail.com>
2023-01-27 17:39:39 +0600

    Time: 307 ms (58.10%), Space: 42.8 MB (81.19%) - LeetHub

--- 0472-concatenated-words/0472-concatenated-words.cpp
+++ 0472-concatenated-words/0472-concatenated-words.cpp
@@ -26,13 +26,15 @@ public:
     vector<string> findAllConcatenatedWordsInADict(vector<string>& words) {
         unordered_set<string> dictionary(words.begin(), words.end());
         vector<string> answer;
+        
         for (const string& word : words) {
-            const int length = word.length();
-            vector<bool> dp(length + 1);
-            dp[0] = true;
+            const int length = word.size();
+            vector<bool> dp(length + 1); dp[0] = true;
+            
             for (int i = 1; i <= length; ++i)
-                for (int j = (i == length ? 1 : 0); !dp[i] && j < i; ++j)
+                for (int j = i == length; !dp[i] && j < i; ++j)
                     dp[i] = dp[j] && dictionary.count(word.substr(j, i - j));
+            
             if (dp[length]) answer.push_back(word);
         }
         return answer;
