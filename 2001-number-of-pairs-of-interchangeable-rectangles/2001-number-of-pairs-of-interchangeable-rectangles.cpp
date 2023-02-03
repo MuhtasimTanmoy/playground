19460cf77f3cf55951c3a7ea9d39ee55b32e2237
T <mtanmoy5086@gmail.com>
2023-02-03 18:19:42 +0600

    Time: 685 ms (33.11%), Space: 117 MB (99.78%) - LeetHub

--- 2001-number-of-pairs-of-interchangeable-rectangles/2001-number-of-pairs-of-interchangeable-rectangles.cpp
+++ 2001-number-of-pairs-of-interchangeable-rectangles/2001-number-of-pairs-of-interchangeable-rectangles.cpp
@@ -1,20 +1,17 @@
 class Solution {
 public:
-    long long interchangeableRectangles(vector<vector<int>>& rectangles) {
+    long long interchangeableRectangles(vector<vector<int>>& r) {
         auto get = [](int w, int h) { return (double) w / h; };
-        sort(rectangles.begin(), rectangles.end(), 
-             [&](const vector<int>& a, const vector<int>& b) {
+        sort(r.begin(), r.end(), [&](const vector<int>& a, const vector<int>& b) {
             return get(a[0], a[1]) < get(b[0], b[1]);
         });
         
         long long res = 0, calc = 1;
-        for (int i = 1; i < rectangles.size(); i++)
-            if (get(rectangles[i][0], rectangles[i][1]) == 
-                get(rectangles[i - 1][0], rectangles[i - 1][1])) {
-                res += calc;
-                calc++;
-            } else 
-                calc = 1;
+        for (int i = 1; i < r.size(); i++) {
+            auto ratioA = get(r[i][0], r[i][1]),
+                 ratioB = get(r[i - 1][0], r[i - 1][1]);
+            if (ratioA == ratioB) res += calc,  calc++; else calc = 1;
+        }
         return res;
     }
 };
