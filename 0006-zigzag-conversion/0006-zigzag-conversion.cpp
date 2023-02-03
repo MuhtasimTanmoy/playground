7b7e02a44b78838b0052555c47d9acffa50154bf
T <mtanmoy5086@gmail.com>
2023-02-03 20:27:46 +0600

    Time: 79 ms (14.95%), Space: 101.9 MB (5.08%) - LeetHub

--- 0006-zigzag-conversion/0006-zigzag-conversion.cpp
+++ 0006-zigzag-conversion/0006-zigzag-conversion.cpp
@@ -7,10 +7,15 @@ public:
         for (auto c: s) {
             vq[i].push(c);
             i += add;
-            if (!i) add = 1;
-            if (i == n - 1 ) add = -1; 
+            add = i == n - 1 ? -1: i? add: 1;
         }
         for (auto q: vq) while (q.size()) res += q.front(), q.pop();
         return res;
     }
+    
+    //  here, this line,
+    //  add = i == n - 1 ? -1: i? add: 1; 
+    //  is shorthand for
+    //  if (i == 0) add = 1;
+    //  else if (i == n - 1 ) add = -1; 
 };
