f92c3343d1b1e81480005673c80e7b1024cca6b9
T <mtanmoy5086@gmail.com>
2023-02-03 20:24:15 +0600

    Time: 108 ms (10.53%), Space: 102.1 MB (5.08%) - LeetHub

--- 0006-zigzag-conversion/0006-zigzag-conversion.cpp
+++ 0006-zigzag-conversion/0006-zigzag-conversion.cpp
@@ -1,19 +1,16 @@
 class Solution {
 public:
-    string convert(string s, int numRows, string res = "") {
-        if (numRows == 1) return s;
-        
-        vector<queue<char>> vq(numRows);
+    string convert(string s, int n, string res = "") {
+        if (n == 1) return s;
+        vector<queue<char>> vq(n);
         int i = 0, add = 1;
         for (auto c: s) {
             vq[i].push(c);
             i += add;
             if (!i) add = 1;
-            if (i == ( numRows - 1 )) add = -1; 
+            if (i == n - 1 ) add = -1; 
         }
-        for (auto q: vq)
-            while (q.size()) 
-                res += q.front(), q.pop();
+        for (auto q: vq) while (q.size()) res += q.front(), q.pop();
         return res;
     }
 };
