f59b24c95e230a3aad072f68091224133518dc0d
T <mtanmoy5086@gmail.com>
2023-02-07 00:26:33 +0600

    Time: 3 ms (42.54%), Space: 6 MB (72.77%) - LeetHub

--- 0556-next-greater-element-iii/0556-next-greater-element-iii.cpp
+++ 0556-next-greater-element-iii/0556-next-greater-element-iii.cpp
@@ -12,7 +12,7 @@ private:
         if (nums.empty()) return false;
         
         int i = nums.size() - 1;
-        while ( i >= 1 && nums[i] <= nums[i-1] ) i--;
+        while ( i && nums[i] <= nums[i-1] ) i--;
         if (i == 0) return false;
         
         int j = nums.size() - 1;
