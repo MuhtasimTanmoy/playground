6501d1bdb5b400e088030ff926287e315635c8a1
T <mtanmoy5086@gmail.com>
2023-02-07 00:25:43 +0600

    Time: 3 ms (42.54%), Space: 5.9 MB (72.77%) - LeetHub

---
+++ 0556-next-greater-element-iii/0556-next-greater-element-iii.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+    int nextGreaterElement(int n) {
+        string nums = to_string(n);
+        if (!nextPermutation(nums)) return -1;
+        
+        long long result=stoll(nums);
+        return (result > INT_MAX || result <= n) ? -1: result;
+    }
+private:
+    bool nextPermutation(string& nums) {
+        if (nums.empty()) return false;
+        
+        int i = nums.size() - 1;
+        while ( i >= 1 && nums[i] <= nums[i-1] ) i--;
+        if (i == 0) return false;
+        
+        int j = nums.size() - 1;
+        while (nums[j] <= nums[i-1]) j--;
+        swap(nums[i-1], nums[j]);
+        
+        reverse(nums.begin() + i, nums.end());
+        return true;
+    }
+};
