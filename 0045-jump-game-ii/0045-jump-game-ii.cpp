dc62fb31e055b85defedabffb43c5bcc5784ecb5
T <mtanmoy5086@gmail.com>
2023-02-08 23:28:17 +0600

    Time: 477 ms (24.23%), Space: 17.3 MB (29.40%) - LeetHub

---
+++ 0045-jump-game-ii/0045-jump-game-ii.cpp
@@ -0,0 +1,31 @@
+// class Solution {
+// public:
+//     int jump(vector<int>& nums) {
+//         unordered_map<int, int> cache;
+        
+//         function<int(int)> go = [&](auto idx) {
+//             if (idx >= (nums.size() - 1)) return 0;
+//             if (cache.count(idx)) return cache[idx];
+            
+//             int res = nums.size();
+//             for (int i = 1; i <= nums[idx]; i++) res = min(res, 1 + go(idx + i));
+//             return nums[idx] = res;
+//         };
+        
+//         return go(0);
+//     }
+// };
+
+
+class Solution {
+public:
+    int jump(vector<int>& nums) {
+        int n = nums.size();
+        vector<int> dp(n, n);
+        dp[0] = 0;
+        for (auto i = 0; i < n; i++) 
+            for (auto j = i + 1; j < min(n, i + nums[i] + 1); j++)
+                dp[j] = min(dp[i] + 1, dp[j]);
+        return dp[n - 1];
+    }
+};
