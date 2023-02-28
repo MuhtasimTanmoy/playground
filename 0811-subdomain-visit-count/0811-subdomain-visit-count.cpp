d3bfc42bf184dad2275fc72a804000a18d3a266a
T <mtanmoy5086@gmail.com>
2023-02-28 17:50:12 +0600

    Time: 16 ms (71.51%), Space: 11.7 MB (33.63%) - LeetHub

---
+++ 0811-subdomain-visit-count/0811-subdomain-visit-count.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+     vector<string> subdomainVisits(vector<string>& cpdomains) {
+        unordered_map<string, int> count;
+        for (auto cd : cpdomains) {
+            int i = cd.find(" ");
+            int n = stoi(cd.substr (0, i));
+            string s = cd.substr (i + 1);
+            for (int i = 0; i < s.size(); ++i)
+                if (s[i] == '.')
+                    count[s.substr(i + 1)] += n;
+            count[s] += n;
+        }
+        vector<string> res;
+        for (auto k : count)
+            res.push_back (to_string(k.second) + " " + k.first);
+        return res;
+    }
+};
