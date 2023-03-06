b323949cd7f6c75e246ca707d912332f896b01e0
MuhtasimTanmoy <taaanmo0oy@gmail.com>
2023-03-07 00:19:59 +0600

    restructure

--- 1539-kth-missing-positive-number/1539-kth-missing-positive-number.cpp
+++
@@ -1,8 +0,0 @@
-class Solution {
-public:
-    int findKthPositive(vector<int>& arr, int k) {
-        int ans = k;
-        for (auto item: arr) { if (ans < item) break; ans++; }
-        return ans;
-    }
-};
--- leetcode/1539-kth-missing-positive-number/1539-kth-missing-positive-number.cpp
+++ leetcode/1539-kth-missing-positive-number/1539-kth-missing-positive-number.cpp
@@ -2,7 +2,7 @@ class Solution {
 public:
     int findKthPositive(vector<int>& arr, int k) {
         int ans = k;
-        for (auto a: arr) if (a <= ans) ans++; else break;
+        for (auto item: arr) { if (ans < item) break; ans++; }
         return ans;
     }
 };
