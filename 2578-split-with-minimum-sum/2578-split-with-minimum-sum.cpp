9e6434fa39d006af515e71f3a748dbc21ee5d03a
T <mtanmoy5086@gmail.com>
2023-03-06 23:00:41 +0600

    Time: 4 ms (100.00%), Space: 6 MB (100.00%) - LeetHub

--- 2578-split-with-minimum-sum/2578-split-with-minimum-sum.cpp
+++ 2578-split-with-minimum-sum/2578-split-with-minimum-sum.cpp
@@ -1,11 +1,8 @@
 class Solution {
 public:
-    int splitNum(int num) {
+    int splitNum(int num, string a = "", string b = "", bool alter = true) {
         auto numstr = to_string(num);
         sort(numstr.begin(), numstr.end());
-        
-        string a = "", b = "";
-        bool alter = true;
         for (auto c: numstr) if (alter = !alter) a.push_back(c); else b.push_back(c);
         return stoi(a) + stoi(b);   
     }
