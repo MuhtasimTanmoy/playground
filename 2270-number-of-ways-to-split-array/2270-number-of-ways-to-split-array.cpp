5d634bee73be22aa69145f2f253f770b29237905
T <mtanmoy5086@gmail.com>
2023-03-07 15:21:33 +0600

    Time: 168 ms (73.84%), Space: 85.4 MB (99.31%) - LeetHub

---
+++ 2270-number-of-ways-to-split-array/2270-number-of-ways-to-split-array.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    int waysToSplitArray(vector<int>& nums) {
+        long long back_sum = accumulate(nums.begin(), nums.end(), 0LL);
+        long long sum = 0, res = 0;
+        for (int i = 0; i < nums.size() - 1; i++) {
+            sum += nums[i];
+            back_sum -= nums[i];
+            if (sum >= back_sum) res++;
+        }
+        return res;
+    }
+};
