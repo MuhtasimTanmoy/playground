604016ba07984bc7af2d738a57bcbebf960fe349
T <mtanmoy5086@gmail.com>
2023-03-09 18:55:59 +0600

    Time: 8 ms (76.50%), Space: 7.6 MB (68.59%) - LeetHub

--- 0142-linked-list-cycle-ii/0142-linked-list-cycle-ii.cpp
+++ 0142-linked-list-cycle-ii/0142-linked-list-cycle-ii.cpp
@@ -6,16 +6,34 @@
  *     ListNode(int x) : val(x), next(NULL) {}
  * };
  */
+// class Solution {
+// public:
+    // ListNode *detectCycle(ListNode *head) {
+    //     unordered_set<ListNode*> cache;
+    //     ListNode* itr = head;
+    //     while (itr && itr->next) {
+    //         if (cache.count(itr)) return itr;
+    //         cache.insert(itr);
+    //         itr = itr->next;
+    //     }
+    //     return NULL;
+    // }
+// };
+
 class Solution {
 public:
     ListNode *detectCycle(ListNode *head) {
-        unordered_set<ListNode*> cache;
-        ListNode* itr = head;
-        while (itr && itr->next) {
-            if (cache.count(itr)) return itr;
-            cache.insert(itr);
-            itr = itr->next;
+        ListNode *slow = head, *fast = head;
+        while (fast && fast->next) {
+            slow = slow->next;
+            fast = fast->next->next;
+            if (slow == fast) break;
         }
-        return NULL;
+        if (!(fast && fast->next)) return NULL;
+        while (head != slow) {
+            head = head->next;
+            slow = slow->next;
+        }
+        return head;
     }
 };
