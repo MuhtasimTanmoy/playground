a0d91138582da6f4a76a2a91d44cd3e1ff7987c4
T <mtanmoy5086@gmail.com>
2023-03-12 17:51:18 +0600

    Time: 167 ms (100.00%), Space: 83.2 MB (25.00%) - LeetHub

--- 2587-rearrange-array-to-maximize-prefix-score/2587-rearrange-array-to-maximize-prefix-score.cpp
+++ 2587-rearrange-array-to-maximize-prefix-score/2587-rearrange-array-to-maximize-prefix-score.cpp
@@ -1,12 +1,8 @@
 class Solution {
 public:
-    int maxScore(vector<int>& nums, int res = 0) {
+    int maxScore(vector<int>& nums, int res = 0, long long pre = 0) {
         sort(nums.begin(), nums.end(), greater<int>());
-        long long i = 0;
-        for (auto num: nums) {
-            i += num;
-            if (i > 0) res++;
-        }
+        for (auto num: nums) pre += num, res += pre > 0;
         return res;
     }
 };
