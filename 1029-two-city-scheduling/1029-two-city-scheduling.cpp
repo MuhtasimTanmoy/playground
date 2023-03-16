73c95df5dfb6c51cb720ed6c76b6b21ca3091103
lucifer <mtanmoy5086@gmail.com>
2023-03-16 15:50:00 +0600

    Time: 82 ms (5.15%), Space: 15.1 MB (5.52%) - LeetHub

---
+++ 1029-two-city-scheduling/1029-two-city-scheduling.cpp
@@ -0,0 +1,38 @@
+// GETS TLE
+// class Solution {
+// public:
+//     int twoCitySchedCost(vector<vector<int>>& costs) {
+//         auto total = costs.size() / 2;
+//         function<int(int, int, int)> go = [&](auto idx, auto a_cnt, auto b_cnt) {
+//             if (idx == total * 2) return 0;
+//             auto cost_a = costs[idx][0], cost_b = costs[idx][1];
+//             auto take_a = a_cnt < total ? cost_a + go(idx + 1, a_cnt + 1, b_cnt): INT_MAX;
+//             auto take_b = b_cnt < total ? cost_b + go(idx + 1, a_cnt, b_cnt + 1): INT_MAX;
+//             return min(take_a, take_b);
+//         };
+//         return go(0, 0, 0);
+//     }
+// };
+
+class Solution {
+public:
+    int twoCitySchedCost(vector<vector<int>>& costs) {
+        auto total = costs.size() / 2;
+        
+        unordered_map<string, int> key_store;
+        auto gen_key = [](int a, int b, int c) { 
+            return to_string(a) + to_string(b) + to_string(c);
+        };
+        
+        function<int(int, int, int)> go = [&](auto idx, auto a_cnt, auto b_cnt) {
+            if (idx == total * 2) return 0;
+            auto key = gen_key(idx, a_cnt, b_cnt);
+            if (key_store.count(key)) return key_store[key];
+            auto cost_a = costs[idx][0], cost_b = costs[idx][1];
+            auto take_a = a_cnt < total ? cost_a + go(idx + 1, a_cnt + 1, b_cnt): INT_MAX;
+            auto take_b = b_cnt < total ? cost_b + go(idx + 1, a_cnt, b_cnt + 1): INT_MAX;
+            return key_store[key] = min(take_a, take_b);
+        };
+        return go(0, 0, 0);
+    }
+};
